9b35a01d531fc6185a3850399b41a5490252ae10
Liang Ding <d@b3log.org>
2022-05-31 10:53:49 +0800

    :art: 每次打开帮助文档时自动检查版本更新并提醒 Fix https://github.com/siyuan-note/siyuan/issues/5057

--- kernel/model/mount.go
+++ kernel/model/mount.go
@@ -179,6 +179,10 @@ func Mount(boxID string) (alreadyMount bool, err error) {
 		go func() {
 			time.Sleep(time.Second * 5)
 			util.PushErrMsg(Conf.Language(52), 9000)
+
+			// 每次打开帮助文档时自动检查版本更新并提醒 https://github.com/siyuan-note/siyuan/issues/5057
+			time.Sleep(time.Second * 10)
+			CheckUpdate(true)
 		}()
 	}
 

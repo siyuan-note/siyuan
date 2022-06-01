53e90d4e29f8f7d9c194feeea59cae2741c741c9
Vanessa <lly219@gmail.com>
2022-06-01 17:11:43 +0800

    :lipstick: fix https://github.com/siyuan-note/siyuan/issues/5069

--- app/src/util/onGetConfig.ts
+++ app/src/util/onGetConfig.ts
@@ -193,7 +193,7 @@ const initBar = () => {
     </svg>
 </button>
 <div class="fn__flex-1 fn__ellipsis" id="drag"><span class="fn__none">开发版，使用前请进行备份 Development version, please backup before use</span></div>
-<div class="fn__flex" style="top: -1px;z-index: 302;right: -1px;position: relative;" id="windowControls"></div>`;
+<div class="fn__flex" style="top: -1px;z-index: 502;right: -1px;position: relative;" id="windowControls"></div>`;
     document.getElementById("barBack").addEventListener("click", () => {
         goBack();
     });

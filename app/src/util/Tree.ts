2360746cc17be3a2ad94bdee548cdf041b550eba
Vanessa <lly219@gmail.com>
2022-06-03 11:01:41 +0800

    :art: https://github.com/siyuan-note/siyuan/issues/5087 1

--- app/src/util/Tree.ts
+++ app/src/util/Tree.ts
@@ -58,8 +58,8 @@ export class Tree {
                 iconHTML = '<svg class="b3-list-item__graphic"><use xlink:href="#iconTags"></use></svg>';
             } else if (item.type === "backlink") {
                 iconHTML = `<svg class="b3-list-item__graphic popover__block" data-id="${item.id}"><use xlink:href="#${getIconByType(item.nodeType, item.subType)}"></use></svg>`;
-            } else if (item.type === "NodeHeading") {
-                iconHTML = `<svg class="b3-list-item__graphic popover__block" data-id="${item.id}"><use xlink:href="#${getIconByType(item.type, item.subType)}"></use></svg>`;
+            } else if (item.type === "outline") {
+                iconHTML = `<svg class="b3-list-item__graphic popover__block" data-id="${item.id}"><use xlink:href="#${getIconByType(item.nodeType, item.subType)}"></use></svg>`;
             }
             let countHTML = "";
             if (item.count) {
@@ -121,7 +121,7 @@ data-def-path="${item.defPath}">
         <svg data-id="${item.id}" class="b3-list-item__arrow${item.children ? "" : " fn__hidden"}"><use xlink:href="#iconRight"></use></svg>
     </span>
     ${iconHTML}
-    <span class="b3-list-item__text">${item.content}</span>
+    <span class="b3-list-item__text" ${type === "outline" ? ' title="' + item.content + '"' : ""}>${item.content}</span>
     ${countHTML}
     ${this.blockExtHTML || ""}
 </li>`;

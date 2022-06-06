fad08e85e649ac00a546f7564b3d905831c01316
Vanessa <lly219@gmail.com>
2022-06-06 21:18:25 +0800

    :bug: fix https://github.com/siyuan-note/siyuan/issues/5111

--- app/src/mobile/index.ts
+++ app/src/mobile/index.ts
@@ -14,7 +14,7 @@ import {initAssets, loadAssets} from "../util/assets";
 import {openMobileFileById} from "./editor";
 import {promiseTransactions} from "../protyle/wysiwyg/transaction";
 import {bootSync} from "../dialog/processSystem";
-import {initMessage, showMessage} from "../dialog/message";
+import {initMessage} from "../dialog/message";
 
 class App {
     constructor() {
@@ -60,8 +60,6 @@ class App {
                         window.JSAndroid.changeStatusBarColor(getComputedStyle(document.body).getPropertyValue("--b3-theme-background"), window.siyuan.config.appearance.mode);
                     }
                     initMessage();
-                    showMessage("ww", 0, "error");
-                    showMessage("dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd", 0)
                 });
             });
             if (navigator.userAgent.indexOf("iPhone") > -1) {

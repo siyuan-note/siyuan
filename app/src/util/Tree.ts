cb55535ef63051b592bfa817b208641e99a72bbc
Vanessa <lly219@gmail.com>
2022-06-07 11:14:50 +0800

    :bug: fix https://github.com/siyuan-note/siyuan/issues/5115

--- app/src/util/Tree.ts
+++ app/src/util/Tree.ts
@@ -76,7 +76,7 @@ ${item.label ? "data-label='" + item.label + "'" : ""}>
         <svg data-id="${encodeURIComponent(item.name + item.depth)}" class="b3-list-item__arrow ${((item.children && item.children.length > 0) || (item.blocks && item.blocks.length > 0)) ? "b3-list-item__arrow--open" : "fn__hidden"}"><use xlink:href="#iconRight"></use></svg>
     </span>
     ${iconHTML}
-    <span class="b3-list-item__text"${item.type === "outline" ? ' title="' + item.name + '"' : ""}>${item.name}</span>
+    <span class="b3-list-item__text"${item.type === "outline" ? ' title="' + Lute.EscapeHTMLStr(item.name) + '"' : ""}>${item.name}</span>
     ${countHTML}
 </li>`;
             if (item.children && item.children.length > 0) {
@@ -121,7 +121,7 @@ data-def-path="${item.defPath}">
         <svg data-id="${item.id}" class="b3-list-item__arrow${item.children ? "" : " fn__hidden"}"><use xlink:href="#iconRight"></use></svg>
     </span>
     ${iconHTML}
-    <span class="b3-list-item__text" ${type === "outline" ? ' title="' + item.content + '"' : ""}>${item.content}</span>
+    <span class="b3-list-item__text" ${type === "outline" ? ' title="' + Lute.EscapeHTMLStr(item.content) + '"' : ""}>${item.content}</span>
     ${countHTML}
     ${this.blockExtHTML || ""}
 </li>`;

81087887b6c4ee2d1540df58444749870b9f1e83
Vanessa <lly219@gmail.com>
2022-06-22 11:58:07 +0800

    :bug: fix https://github.com/siyuan-note/siyuan/issues/5262

--- app/src/menus/commonMenuItem.ts
+++ app/src/menus/commonMenuItem.ts
@@ -265,6 +265,7 @@ export const openFileAttr = (attrs: IObject, id: string, focusName = "bookmark")
                         });
                     }
                     window.siyuan.menus.menu.element.classList.remove("fn__none");
+                    window.siyuan.menus.menu.element.style.zIndex = "310";
                     setPosition(window.siyuan.menus.menu.element, event.clientX, event.clientY + 16);
                 });
                 break;
@@ -460,6 +461,7 @@ export const openAttr = (nodeElement: Element, protyle: IProtyle, focusName = "b
                             });
                         }
                         window.siyuan.menus.menu.element.classList.remove("fn__none");
+                        window.siyuan.menus.menu.element.style.zIndex = "310";
                         setPosition(window.siyuan.menus.menu.element, event.clientX, event.clientY + 16);
                     });
                     break;

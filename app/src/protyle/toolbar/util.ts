d982fd1ddec5a7a7914bf00cc749960223d29e8e
Vanessa <lly219@gmail.com>
2022-08-18 20:13:03 +0800

    :art: https://github.com/siyuan-note/siyuan/issues/2892

--- app/src/protyle/toolbar/util.ts
+++ app/src/protyle/toolbar/util.ts
@@ -1,7 +1,7 @@
 import {fetchPost} from "../../util/fetch";
 
 export const previewTemplate = (pathString: string, element: Element) => {
-    fetchPost("/api/file/getFile", {path: pathString.replace(window.siyuan.config.system.dataDir, "")}, (response) => {
+    fetchPost("/api/file/getFile", {path: pathString.replace(window.siyuan.config.system.dataDir.substring(0, window.siyuan.config.system.dataDir.length - 4), "")}, (response) => {
         element.innerHTML = response.data;
     })
 }

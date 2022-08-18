e2705bc2ac1a6dbafee46aac7a1397063357b271
Vanessa <lly219@gmail.com>
2022-08-18 20:13:44 +0800

    :art: https://github.com/siyuan-note/siyuan/issues/2892

--- app/src/protyle/toolbar/util.ts
+++ app/src/protyle/toolbar/util.ts
@@ -2,6 +2,6 @@ import {fetchPost} from "../../util/fetch";
 
 export const previewTemplate = (pathString: string, element: Element) => {
     fetchPost("/api/file/getFile", {path: pathString.replace(window.siyuan.config.system.dataDir.substring(0, window.siyuan.config.system.dataDir.length - 4), "")}, (response) => {
-        element.innerHTML = response.data;
+        element.innerHTML = `<div class="b3-typography">${response.data}</div>`;
     })
 }

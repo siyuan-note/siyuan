6b34eb325f829f856230ce03bde07b97f35a68b5
Vanessa <lly219@gmail.com>
2022-08-26 22:12:45 +0800

    :bug: fix https://github.com/siyuan-note/siyuan/issues/5729

--- app/src/protyle/util/onGet.ts
+++ app/src/protyle/util/onGet.ts
@@ -119,7 +119,7 @@ export const onGet = (data: IWebSocketData, protyle: IProtyle, action: string[] 
             unScroll: (scrollObj && scrollObj.focusId) ? true : false,
         }, protyle);
 
-        if (scrollObj && protyle.options.mode === "wysiwyg") {
+        if (scrollObj && protyle.options.mode !== "preview") {
             restoreScroll(protyle, scrollObj);
         }
         const loadingElement = protyle.element.querySelector(".fn__loading");

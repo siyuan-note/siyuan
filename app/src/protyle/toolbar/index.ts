86095cdbb9d20fdbbf4004c0871eba49905477f8
Vanessa <lly219@gmail.com>
2022-09-19 08:43:28 +0800

    :bug: fix https://github.com/siyuan-note/insider/issues/1044

--- app/src/protyle/toolbar/index.ts
+++ app/src/protyle/toolbar/index.ts
@@ -234,7 +234,7 @@ export class Toolbar {
     }
 
     public setInlineMark(protyle: IProtyle, type: string, action: "range" | "toolbar", textObj?: ITextOption) {
-        if (["text", "a", "block-ref", "inline-math", "inline-memo"].includes(type)) {
+        if (["a", "block-ref", "inline-math", "inline-memo"].includes(type)) {
             protyle.toolbar.element.querySelector(`[data-type="${type}"]`).dispatchEvent(new CustomEvent("block-ref" === type ? getEventName() : "click"));
             return;
         }

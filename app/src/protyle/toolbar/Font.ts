e4a9bad48b6c7bc3afb2ecfdc43549867733e271
Vanessa <lly219@gmail.com>
2022-09-21 15:14:50 +0800

    :sparkles: fix https://github.com/siyuan-note/siyuan/issues/5914

--- app/src/protyle/toolbar/Font.ts
+++ app/src/protyle/toolbar/Font.ts
@@ -79,6 +79,23 @@ export const fontMenu = (protyle: IProtyle) => {
 <div class="fn__flex">
     <button data-type="style2" class="protyle-font__style" style="-webkit-text-stroke: 0.2px var(--b3-theme-on-background);-webkit-text-fill-color : transparent;">${window.siyuan.languages.hollow}</button>
     <button data-type="style4" class="protyle-font__style" style="text-shadow: 1px 1px var(--b3-border-color), 2px 2px var(--b3-border-color), 3px 3px var(--b3-border-color), 4px 4px var(--b3-border-color)">${window.siyuan.languages.shadow}</button>
+</div>
+<div style="margin: 4px 0 2px">${window.siyuan.languages.fontSize}</div>
+<div class="fn__flex">
+    <select class="b3-select">
+        <option value="12px">12px</option>
+        <option value="13px">13px</option>
+        <option value="14px">14px</option>
+        <option value="15px">15px</option>
+        <option selected value="16px">16px</option>
+        <option value="19px">19px</option>
+        <option value="22px">22px</option>
+        <option value="24px">24px</option>
+        <option value="29px">29px</option>
+        <option value="32px">32px</option>
+        <option value="40px">40px</option>
+        <option value="48px">48px</option>
+    </select>
 </div>`;
     element.addEventListener(getEventName(), function (event: Event) {
         let target = event.target as HTMLElement;
@@ -90,6 +107,9 @@ export const fontMenu = (protyle: IProtyle) => {
             target = target.parentElement;
         }
     });
+    element.querySelector("select").addEventListener("change", function (event: Event) {
+        fontEvent(protyle, "fontSize", (event.target as HTMLSelectElement).value);
+    })
     return element;
 };
 

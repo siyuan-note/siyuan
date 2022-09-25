5bc259b0983921a18b9c5e3ba482024c15a4cc98
Liang Ding <d@b3log.org>
2022-09-25 19:04:42 +0800

    :art: 带子级升降标题层级 https://github.com/siyuan-note/siyuan/issues/2860

--- kernel/api/router.go
+++ kernel/api/router.go
@@ -147,6 +147,7 @@ func ServeAPI(ginServer *gin.Engine) {
 	ginServer.Handle("POST", "/api/block/updateBlock", model.CheckAuth, updateBlock)
 	ginServer.Handle("POST", "/api/block/deleteBlock", model.CheckAuth, deleteBlock)
 	ginServer.Handle("POST", "/api/block/setBlockReminder", model.CheckAuth, setBlockReminder)
+	ginServer.Handle("POST", "/api/block/getHeadingLevelTransaction", model.CheckAuth, getHeadingLevelTransaction)
 
 	ginServer.Handle("POST", "/api/file/getFile", model.CheckAuth, getFile)
 	ginServer.Handle("POST", "/api/file/putFile", model.CheckAuth, putFile)

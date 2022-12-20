9cf02cb629bceb6b5e787c1432b1bb93a0401c60
Liang Ding <d@b3log.org>
2022-12-20 20:50:50 +0800

    :art: 桌面端检查磁盘可用空间 https://github.com/siyuan-note/siyuan/issues/6873

--- kernel/util/disk_mobile.go
+++ kernel/util/disk_mobile.go
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-//go:build ios
+//go:build ios || android
 
 package util
 

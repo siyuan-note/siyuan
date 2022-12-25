a96d6a019f42a775bdcc0ac076b893140379f557
Liang Ding <d@b3log.org>
2022-12-25 10:14:02 +0800

    :bug: Windows 端设置开机启动失效 https://github.com/siyuan-note/siyuan/issues/6920

--- kernel/api/router.go
+++ kernel/api/router.go
@@ -40,6 +40,7 @@ func ServeAPI(ginServer *gin.Engine) {
 	ginServer.Handle("POST", "/api/system/setAccessAuthCode", model.CheckAuth, setAccessAuthCode)
 	ginServer.Handle("POST", "/api/system/setNetworkServe", model.CheckAuth, setNetworkServe)
 	ginServer.Handle("POST", "/api/system/setUploadErrLog", model.CheckAuth, setUploadErrLog)
+	ginServer.Handle("POST", "/api/system/setAutoLaunch", model.CheckAuth, setAutoLaunch)
 	ginServer.Handle("POST", "/api/system/setGoogleAnalytics", model.CheckAuth, setGoogleAnalytics)
 	ginServer.Handle("POST", "/api/system/setDownloadInstallPkg", model.CheckAuth, setDownloadInstallPkg)
 	ginServer.Handle("POST", "/api/system/setNetworkProxy", model.CheckAuth, setNetworkProxy)

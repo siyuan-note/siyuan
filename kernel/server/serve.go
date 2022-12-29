e06d774da0abf9e2f78c72fe3c92e4b8f1139ce2
Liang Ding <d@b3log.org>
2022-12-29 22:48:06 +0800

    :art: 尝试解决潜在的剪藏扩展请求跨域问题 https://github.com/siyuan-note/siyuan/issues/6907

--- kernel/server/serve.go
+++ kernel/server/serve.go
@@ -474,6 +474,7 @@ func corsMiddleware() gin.HandlerFunc {
 		c.Header("Access-Control-Allow-Credentials", "true")
 		c.Header("Access-Control-Allow-Headers", "origin, Content-Length, Content-Type, Authorization")
 		c.Header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, HEAD, OPTIONS")
+		c.Header("Access-Control-Allow-Private-Network", "true")
 
 		if c.Request.Method == "OPTIONS" {
 			c.AbortWithStatus(204)

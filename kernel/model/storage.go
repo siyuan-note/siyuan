7688733364fc3d8962a7ef27ed0cf3bd2e85d1a2
Liang Ding <d@b3log.org>
2022-12-31 15:59:33 +0800

    :art: 保存命名查询去掉多余的字段

--- kernel/model/storage.go
+++ kernel/model/storage.go
@@ -157,19 +157,16 @@ func getRecentDocs() (ret []*RecentDoc, err error) {
 }
 
 type Criterion struct {
-	Name        string          `json:"name"`
-	Sort        int             `json:"sort"`       //  0：按块类型（默认），1：按创建时间升序，2：按创建时间降序，3：按更新时间升序，4：按更新时间降序，5：按内容顺序（仅在按文档分组时）
-	Group       int             `json:"group"`      // 0：不分组，1：按文档分组
-	Layout      int             `json:"layout"`     // 0：上下，1：左右
-	HasReplace  bool            `json:"hasReplace"` // 是否有替换
-	Method      int             `json:"method"`     //  0：文本，1：查询语法，2：SQL，3：正则表达式
-	HPath       string          `json:"hPath"`
-	IDPath      []string        `json:"idPath"`
-	K           string          `json:"k"`           // 搜索关键字
-	R           string          `json:"r"`           // 替换关键字
-	ReplaceList []string        `json:"replaceList"` // 替换候选列表
-	List        []string        `json:"list"`        // 搜索候选列表
-	Types       *CriterionTypes `json:"types"`       // 类型过滤选项
+	Name       string          `json:"name"`
+	Sort       int             `json:"sort"`       //  0：按块类型（默认），1：按创建时间升序，2：按创建时间降序，3：按更新时间升序，4：按更新时间降序，5：按内容顺序（仅在按文档分组时）
+	Group      int             `json:"group"`      // 0：不分组，1：按文档分组
+	HasReplace bool            `json:"hasReplace"` // 是否有替换
+	Method     int             `json:"method"`     //  0：文本，1：查询语法，2：SQL，3：正则表达式
+	HPath      string          `json:"hPath"`
+	IDPath     []string        `json:"idPath"`
+	K          string          `json:"k"`     // 搜索关键字
+	R          string          `json:"r"`     // 替换关键字
+	Types      *CriterionTypes `json:"types"` // 类型过滤选项
 }
 
 type CriterionTypes struct {

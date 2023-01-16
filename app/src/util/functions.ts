f49ebee0e81e8df2af5fa5bd862506ba94cfd693
Vanessa <lly219@gmail.com>
2023-01-17 00:40:51 +0800

    :recycle: https://github.com/siyuan-note/siyuan/pull/7089/files

--- app/src/util/functions.ts
+++ app/src/util/functions.ts
@@ -14,10 +14,20 @@ export const getRandom = (min: number, max: number) => {
     return Math.floor(Math.random() * (max - min + 1)) + min; //含最大值，含最小值
 };
 
-export const getSearch: (key: string, link?: string) => string | null = (key: string, link = window.location.search) => {
-    // REF https://developer.mozilla.org/zh-CN/docs/Web/API/URLSearchParams
-    const urlSearchParams = new URLSearchParams(link);
-    return urlSearchParams.get(key);
+export const getSearch = (key: string, link = window.location.search) => {
+    if (link.indexOf("?") === -1) {
+        return "";
+    }
+    let value = "";
+    const data = link.split("?")[1].split("&");
+    data.find(item => {
+        const keyValue = item.split("=");
+        if (keyValue[0] === key) {
+            value = keyValue[1];
+            return true;
+        }
+    });
+    return value;
 };
 
 export const isBrowser = () => {

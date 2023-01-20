a5dcf0827f5b35a954d2e7df855c6b44c7804a2e
Vanessa <lly219@gmail.com>
2023-01-20 11:58:30 +0800

    :art: fix https://github.com/siyuan-note/siyuan/issues/7134

--- app/src/protyle/preview/image.ts
+++ app/src/protyle/preview/image.ts
@@ -19,6 +19,14 @@ export const previewImage = (src: string, id: string) => {
             imagesElement.innerHTML = html;
             // @ts-ignore
             const viewer = new Viewer(imagesElement, {
+                title: [1, (image: HTMLImageElement, imageData: IObject) => {
+                    let name = image.alt;
+                    if (!name) {
+                        name = image.src.substring(image.src.lastIndexOf("/") + 1);
+                    }
+                    name = name.substring(0, name.lastIndexOf(".")).replace(/-\d{14}-\w{7}$/, "");
+                    return `${name} [${imageData.naturalWidth} × ${imageData.naturalHeight}]`;
+                }],
                 button: false,
                 initialViewIndex,
                 transition: false,

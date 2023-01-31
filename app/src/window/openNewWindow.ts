d812df99f7f75d8cab18e2c0ca2dd0b95c54e8cc
Vanessa <lly219@gmail.com>
2023-01-31 21:10:37 +0800

    :art: https://github.com/siyuan-note/siyuan/issues/7206

--- app/src/window/openNewWindow.ts
+++ app/src/window/openNewWindow.ts
@@ -8,6 +8,8 @@ import {Tab} from "../layout/Tab";
 export const openNewWindow = (tab: Tab) => {
     const json = {};
     layoutToJSON(tab, json);
+    /// #if !BROWSER
     ipcRenderer.send(Constants.SIYUAN_OPENWINDOW, `${window.location.protocol}//${window.location.host}/stage/build/app/window.html?v=${Constants.SIYUAN_VERSION}&json=${JSON.stringify(json)}`);
+    /// #endif
     tab.parent.removeTab(tab.id);
 };

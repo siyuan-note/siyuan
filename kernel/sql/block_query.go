891541ad1b6c157a47a6d31962cca56b33875815
Liang Ding <d@b3log.org>
2023-02-01 14:43:04 +0800

    :mute: logging

--- kernel/sql/block_query.go
+++ kernel/sql/block_query.go
@@ -478,7 +478,7 @@ func selectBlocksRawStmt(stmt string, limit int) (ret []*Block) {
 			errCount++
 		}
 
-		if (confLimit && limit < count) || 2 < errCount {
+		if (confLimit && limit < count) || 1 < errCount {
 			break
 		}
 	}

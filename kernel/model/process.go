b120e66ca976399f8b110a326e96e1e9f2bcf203
Liang Ding <d@b3log.org>
2023-02-12 09:21:14 +0800

    :bug: 桌面端系统睡眠唤醒后内核可能会自动退出 Fix https://github.com/siyuan-note/siyuan/issues/7323

--- kernel/model/process.go
+++ kernel/model/process.go
@@ -119,9 +119,10 @@ func getAttachedUIProcCount() (ret int) {
 
 // getUIProcCount 获取 UI 进程数。
 func getUIProcCount() (ret int) {
+	pid := os.Getpid()
 	procs, _ := goPS.Processes()
 	for _, proc := range procs {
-		if proc.Pid() == os.Getpid() {
+		if proc.Pid() == pid {
 			continue
 		}
 

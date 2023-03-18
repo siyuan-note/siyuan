3993b96e54780e5880bede9d603f297bd9b5fd70
Liang Ding <d@b3log.org>
2023-03-18 17:38:58 +0800

    :art: 编辑器启用只读模式时启动后提示用户 Fix https://github.com/siyuan-note/siyuan/issues/7700

--- kernel/api/system.go
+++ kernel/api/system.go
@@ -168,7 +168,7 @@ func getConf(c *gin.Context) {
 		if model.Conf.Editor.ReadOnly {
 			// 编辑器启用只读模式时启动后提示用户 https://github.com/siyuan-note/siyuan/issues/7700
 			go func() {
-				time.Sleep(time.Second * 5)
+				time.Sleep(time.Second * 7)
 				if model.Conf.Editor.ReadOnly {
 					util.PushMsg(model.Conf.Language(197), 7000)
 				}

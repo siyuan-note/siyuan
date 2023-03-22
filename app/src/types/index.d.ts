fd38d4e1f0510935be93b3472d4ec5233c28b2f0
Vanessa <lly219@gmail.com>
2023-03-22 19:18:23 +0800

    :art: fix https://github.com/siyuan-note/siyuan/issues/7745

--- app/src/types/index.d.ts
+++ app/src/types/index.d.ts
@@ -81,6 +81,7 @@ interface ICard {
 }
 
 interface ISearchOption {
+    removed?: boolean  // 移除后需记录搜索内容 https://github.com/siyuan-note/siyuan/issues/7745
     name?: string
     sort: number,  //  0：按块类型（默认），1：按创建时间升序，2：按创建时间降序，3：按更新时间升序，4：按更新时间降序，5：按内容顺序（仅在按文档分组时），6：按相关度升序，7：按相关度降序
     group: number,  // 0：不分组，1：按文档分组

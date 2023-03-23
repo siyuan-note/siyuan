8e1228f8b6796934ddefc62eb73f003e7094c4ec
Liang Ding <d@b3log.org>
2023-03-23 19:55:01 +0800

    :art: 识别移动端平台版本

--- kernel/util/os_mobile.go
+++ kernel/util/os_mobile.go
@@ -20,10 +20,10 @@ package util
 
 func GetOSPlatform() (plat, ver string) {
 	if ContainerIOS == Container {
-		return "iOS", MobileOSVer
+		return "iOS " + MobileOSVer
 	}
 	if ContainerAndroid == Container {
-		return "Android", MobileOSVer
+		return "Android " + MobileOSVer
 	}
-	return "Unknown", MobileOSVer
+	return "Unknown " + MobileOSVer
 }

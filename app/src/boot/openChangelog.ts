faafc9f837920e68490517961973d9bcbf847603
Vanessa <lly219@gmail.com>
2023-04-06 23:05:34 +0800

    :lipstick:

--- app/src/boot/openChangelog.ts
+++ app/src/boot/openChangelog.ts
@@ -10,7 +10,7 @@ export const openChangelog = () => {
         }
         const dialog = new Dialog({
             title: `✨ ${window.siyuan.languages.changelog}`,
-            width: isMobile() ? "80vw" : "768px",
+            width: isMobile() ? "90vw" : "768px",
             content: `<div style="overflow:auto;height: ${isMobile() ? "80" : "70"}vh;" class="b3-dialog__content b3-typography b3-typography--default">${response.data.html}</div>`
         });
         highlightRender(dialog.element);

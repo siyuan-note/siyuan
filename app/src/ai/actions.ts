7b34ca08554b6676b5b8149a479f0f6c270e2281
颖逸 <49649786+Zuoqiu-Yingyi@users.noreply.github.com>
2023-04-06 08:34:31 +0800

    AI translation adds support for `Traditional Chinese`
    * :art: AI translation support `Traditional Chinese`
    
    * Update actions.ts

--- app/src/ai/actions.ts
+++ app/src/ai/actions.ts
@@ -155,11 +155,22 @@ export const AIActions = (elements: Element[], protyle: IProtyle) => {
             type: "submenu",
             submenu: [{
                 iconHTML: Constants.ZWSP,
-                label: window.siyuan.languages.aiTranslate_zh_CN,
+                label: window.siyuan.languages.aiTranslate_zh_Hans,
                 click() {
                     fetchPost("/api/ai/chatGPTWithAction", {
                         ids,
-                        action: "Translate as follows to [zh_CN]"
+                        action: "Translate as follows to [zh-Hans]"
+                    }, (response) => {
+                        fillContent(protyle, response.data, elements);
+                    });
+                }
+            }, {
+                iconHTML: Constants.ZWSP,
+                label: window.siyuan.languages.aiTranslate_zh_Hant,
+                click() {
+                    fetchPost("/api/ai/chatGPTWithAction", {
+                        ids,
+                        action: "Translate as follows to [zh-Hant]"
                     }, (response) => {
                         fillContent(protyle, response.data, elements);
                     });
@@ -170,10 +181,9 @@ export const AIActions = (elements: Element[], protyle: IProtyle) => {
                 click() {
                     fetchPost("/api/ai/chatGPTWithAction", {
                         ids,
-                        action: "Translate as follows to [ja_JP]"
+                        action: "Translate as follows to [ja-JP]"
                     }, (response) => {
-                        focusByRange(protyle.toolbar.range);
-                        insertHTML(response.data, protyle, true);
+                        fillContent(protyle, response.data, elements);
                     });
                 }
             }, {
@@ -182,7 +192,7 @@ export const AIActions = (elements: Element[], protyle: IProtyle) => {
                 click() {
                     fetchPost("/api/ai/chatGPTWithAction", {
                         ids,
-                        action: "Translate as follows to [ko_KR]"
+                        action: "Translate as follows to [ko-KR]"
                     }, (response) => {
                         fillContent(protyle, response.data, elements);
                     });
@@ -193,7 +203,7 @@ export const AIActions = (elements: Element[], protyle: IProtyle) => {
                 click() {
                     fetchPost("/api/ai/chatGPTWithAction", {
                         ids,
-                        action: "Translate as follows to [en_US]"
+                        action: "Translate as follows to [en-US]"
                     }, (response) => {
                         fillContent(protyle, response.data, elements);
                     });
@@ -204,7 +214,7 @@ export const AIActions = (elements: Element[], protyle: IProtyle) => {
                 click() {
                     fetchPost("/api/ai/chatGPTWithAction", {
                         ids,
-                        action: "Translate as follows to [es_ES]"
+                        action: "Translate as follows to [es-ES]"
                     }, (response) => {
                         fillContent(protyle, response.data, elements);
                     });
@@ -215,7 +225,7 @@ export const AIActions = (elements: Element[], protyle: IProtyle) => {
                 click() {
                     fetchPost("/api/ai/chatGPTWithAction", {
                         ids,
-                        action: "Translate as follows to [fr_FR]"
+                        action: "Translate as follows to [fr-FR]"
                     }, (response) => {
                         fillContent(protyle, response.data, elements);
                     });
@@ -226,7 +236,7 @@ export const AIActions = (elements: Element[], protyle: IProtyle) => {
                 click() {
                     fetchPost("/api/ai/chatGPTWithAction", {
                         ids,
-                        action: "Translate as follows to [de_DE]"
+                        action: "Translate as follows to [de-DE]"
                     }, (response) => {
                         fillContent(protyle, response.data, elements);
                     });

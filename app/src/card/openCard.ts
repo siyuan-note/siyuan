7d8f725d8a12d28410ebc1b626372d90a161a308
Vanessa <lly219@gmail.com>
2023-04-14 16:44:42 +0800

    :art: https://github.com/siyuan-note/siyuan/issues/7954

--- app/src/card/openCard.ts
+++ app/src/card/openCard.ts
@@ -222,7 +222,7 @@ export const openCardByData = (cardsData: {
                         label: window.siyuan.languages.fileTree,
                         click() {
                             movePathTo((toPath, toNotebook) => {
-                                filterElement.setAttribute("data-id", toPath[0] === "/" ? toNotebook[0] : getDisplayName(toPath[0], true))
+                                filterElement.setAttribute("data-id", toPath[0] === "/" ? toNotebook[0] : getDisplayName(toPath[0], true, true))
                                 filterElement.setAttribute("data-cardtype", toPath[0] === "/" ? "notebook" : "doc")
                                 fetchNewRound();
                             }, [], undefined, window.siyuan.languages.specifyPath, true)

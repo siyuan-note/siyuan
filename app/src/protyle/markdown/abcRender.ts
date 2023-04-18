93e0e1783f365da62ba38df82794f5f3d52b08af
Vanessa <lly219@gmail.com>
2023-04-18 09:16:55 +0800

    :arrow_up: https://github.com/siyuan-note/siyuan/issues/8013

--- app/src/protyle/markdown/abcRender.ts
+++ app/src/protyle/markdown/abcRender.ts
@@ -17,7 +17,7 @@ export const abcRender = (element: Element, cdn = Constants.PROTYLE_CDN) => {
         return;
     }
     if (abcElements.length > 0) {
-        addScript(`${cdn}/js/abcjs/abcjs-basic-min.js?v=6.2.1`, "protyleAbcjsScript").then(() => {
+        addScript(`${cdn}/js/abcjs/abcjs-basic-min.js?v=6.2.2`, "protyleAbcjsScript").then(() => {
             abcElements.forEach((e: HTMLDivElement) => {
                 if (e.getAttribute("data-render") === "true") {
                     return;

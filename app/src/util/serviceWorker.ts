7e0716eafc023587342d17e407ef61c3c237e0f1
Vanessa <lly219@gmail.com>
2023-04-19 18:56:48 +0800

    :rotating_light:

--- app/src/util/serviceWorker.ts
+++ app/src/util/serviceWorker.ts
@@ -1,7 +1,7 @@
 // https://github.com/siyuan-note/siyuan/pull/8012
 export const registerServiceWorker = (scriptURL: string) => {
-    if (!"serviceWorker" in navigator || typeof (navigator.serviceWorker) === "undefined" ||
-        !"caches" in window || !"fetch" in window) {
+    if (!("serviceWorker" in navigator) || typeof (navigator.serviceWorker) === "undefined" ||
+        !("caches" in window) || !("fetch" in window)) {
         return;
     }
     // REF https://developer.mozilla.org/en-US/docs/Web/API/ServiceWorkerRegistration

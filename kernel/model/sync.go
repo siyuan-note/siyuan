98e7c29b379059540ac0076a731bc0a13a241809
Liang Ding <d@b3log.org>
2023-04-24 15:44:43 +0800

    :art: Error notification pops up only once after auto sync fails https://github.com/siyuan-note/siyuan/issues/8096

--- kernel/model/sync.go
+++ kernel/model/sync.go
@@ -578,11 +578,12 @@ func isProviderOnline(byHand bool) (ret bool) {
 	}
 
 	if ret = util.IsOnline(checkURL, skipTlsVerify); !ret {
-		if !byHand {
-			autoSyncErrCount++
-		}
 		if 1 > autoSyncErrCount || byHand {
 			util.PushErrMsg(Conf.Language(76)+" (Provider: "+conf.ProviderToStr(Conf.Sync.Provider)+")", 5000)
+		}
+		if !byHand {
+			planSyncAfter(30 * time.Second)
+			autoSyncErrCount++
 		}
 	}
 	return

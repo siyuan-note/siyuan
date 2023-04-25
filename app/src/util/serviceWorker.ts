99c4f1f8909878a288156e80a47d57a7c35c6341
颖逸 <49649786+Zuoqiu-Yingyi@users.noreply.github.com>
2023-04-25 10:29:26 +0800

    Improve Service Worker (#8089)
    * Update service-worker.js
    
    * Update serviceWorker.ts
    
    * Update serviceWorker.ts

--- app/src/util/serviceWorker.ts
+++ app/src/util/serviceWorker.ts
@@ -1,5 +1,5 @@
 // https://github.com/siyuan-note/siyuan/pull/8012
-export const registerServiceWorker = (scriptURL: string) => {
+export const registerServiceWorker = (scriptURL: string, scope: string = "/", workerType: WorkerType = "module") => {
     if (!("serviceWorker" in navigator) || typeof (navigator.serviceWorker) === "undefined" ||
         !("caches" in window) || !("fetch" in window)) {
         return;
@@ -7,8 +7,8 @@ export const registerServiceWorker = (scriptURL: string) => {
     // REF https://developer.mozilla.org/en-US/docs/Web/API/ServiceWorkerRegistration
     window.navigator.serviceWorker
         .register(scriptURL, {
-            scope: "./",
-            type: "module",
+            scope,
+            type: workerType,
         }).then(registration => {
         registration.update();
     }).catch(e => {

19a01e862d28605bf0289e321a06a1e1ae1b5112
Vanessa <lly219@gmail.com>
2023-04-30 23:08:47 +0800

    :art: fix https://github.com/siyuan-note/siyuan/issues/8148

--- app/src/protyle/header/Background.ts
+++ app/src/protyle/header/Background.ts
@@ -26,7 +26,7 @@ export class Background {
         this.element = document.createElement("div");
         this.element.className = "protyle-background";
         this.element.innerHTML = `<div class="protyle-background__img">
-    <img class="fn__none" style="isMobile()? "200px" : "30vh"">
+    <img class="fn__none">
     <div class="protyle-icons">
         <span class="protyle-icon protyle-icon--first b3-tooltips b3-tooltips__sw" style="position: relative" aria-label="${window.siyuan.languages.upload}"><input type="file" style="position: absolute;width: 22px;height: 100%;top: 0;left: 0;opacity: .001;overflow: hidden;cursor: pointer;"><svg><use xlink:href="#iconUpload"></use></svg></span>
         <span class="protyle-icon b3-tooltips b3-tooltips__sw" data-type="link" aria-label="${window.siyuan.languages.link}"><svg><use xlink:href="#iconLink"></use></svg></span>

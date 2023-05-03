e4ea1a782444d8a96cc6d277b701af77b2509e1e
Liang Ding <d@b3log.org>
2023-05-03 14:35:07 +0800

    :art: `Copy block hyperlink (Markdown)` anchor text length follows editor setting `The maximum length of block ref dynamic anchor text` Fix https://github.com/siyuan-note/siyuan/issues/8144

--- app/src/menus/commonMenuItem.ts
+++ app/src/menus/commonMenuItem.ts
@@ -473,7 +473,7 @@ export const copySubMenu = (id: string, accelerator = true, focusElement?: Eleme
         label: window.siyuan.languages.copyProtocolInMd,
         click: () => {
             fetchPost("/api/block/getRefText", {id}, (response) => {
-                writeText(`[${response.data.substring(0, Constants.SIZE_LINK_TEXT_MAX)}](siyuan://blocks/${id})`);
+                writeText(`[${response.data}](siyuan://blocks/${id})`);
             });
             if (focusElement) {
                 focusBlock(focusElement);

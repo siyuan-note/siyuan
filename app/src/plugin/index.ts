2ebc2659961e118247e74f5b06420c469ead956f
Vanessa <lly219@gmail.com>
2023-05-06 11:29:46 +0800

    :art: https://github.com/siyuan-note/siyuan/issues/5066

--- app/src/plugin/index.ts
+++ app/src/plugin/index.ts
@@ -1,10 +1,17 @@
+import {App} from "../index";
+
 export class Plugin {
+    public i18n: IObject;
 
-    constructor() {
-
+    constructor(options: {
+        app: App,
+        id: string,
+        i18n: IObject
+    }) {
+        this.i18n = options.i18n;
     }
 
-    public getData  () {
+    public getData() {
 
     }
 

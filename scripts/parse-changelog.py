3f978c798709b8bf4f65107a11d12953afbcec7b
Daniel <845765@qq.com>
2023-05-22 09:12:44 +0800

    :octocat: Update CD action

--- scripts/parse-changelog.py
+++ scripts/parse-changelog.py
@@ -60,14 +60,11 @@ def find_milestone(repo, title, lastestRelease):
         return None
     version = pat.group(1)
     print(f'''
----
-<p align="center">
+<p>
 <a href="https://github.com/siyuan-note/siyuan/actions/workflows/ci.yml"><img src="https://img.shields.io/github/actions/workflow/status/siyuan-note/siyuan/ci.yml?event=push&label=ci.yml%20Action&logo=github" style="cursor:pointer;height: 30px;margin: 3px auto;"/></a>
 <a href="https://github.com/siyuan-note/siyuan/releases/{thisRelease}/"><img src="https://img.shields.io/github/downloads/siyuan-note/siyuan/{thisRelease}/total?logo=github" style="cursor:pointer;height: 30px;margin: 3px auto;"/></a>
 <img alt="GitHub commits difference between two branches/tags/commits" src="https://img.shields.io/github/commits-difference/siyuan-note/siyuan?base={lastestRelease}&head={thisRelease}&logo=git" style="cursor:pointer;height: 30px;margin: 3px auto;"/>
 </p>
-
----
 
 ''')
     for milestone in repo.get_milestones():

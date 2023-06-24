385c82303037935dc3732d2ef1b8213e5abcb244
Vanessa <lly219@gmail.com>
2023-06-24 17:50:51 +0800

    :art: https://github.com/siyuan-note/siyuan/issues/8526

---
+++ app/src/plugin/commandPanel.ts
@@ -0,0 +1,8 @@
+import {Dialog} from "../dialog";
+
+export const commandPanel = () => {
+    const panel = new Dialog({
+        title: window.siyuan.languages.commandPanel,
+        content:`<div></div>`
+    })
+}

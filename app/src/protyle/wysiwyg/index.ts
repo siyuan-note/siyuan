7e1d5c0a26aa577b8524653353076f776640edee
Vanessa <lly219@gmail.com>
2023-08-01 21:42:57 +0800

    :iphone: fix https://github.com/siyuan-note/siyuan/issues/8864

--- app/src/protyle/wysiwyg/index.ts
+++ app/src/protyle/wysiwyg/index.ts
@@ -2019,6 +2019,7 @@ export class WYSIWYG {
                     } else if (lastEditElement) {
                         range.selectNodeContents(lastEditElement);
                         range.collapse(false);
+                        focusByRange(range);
                     }
                 }
             }

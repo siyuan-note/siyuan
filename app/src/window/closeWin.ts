dc92bfdc8f20b675ed08116e5d88ff6e8f2564e7
Vanessa <lly219@gmail.com>
2023-09-16 19:32:00 +0800

    :art: fix https://github.com/siyuan-note/siyuan/issues/9128

---
+++ app/src/window/closeWin.ts
@@ -0,0 +1,9 @@
+import {App} from "../index";
+import {getCurrentWindow} from "@electron/remote";
+
+export const closeWindow = async (app: App) => {
+    for (let i = 0; i < app.plugins.length; i++) {
+        await app.plugins[i].onunload();
+    }
+    getCurrentWindow().destroy();
+}

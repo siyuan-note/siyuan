8f2c5c3325e3e1e9f65eb66df8e668baca43f8bb
Daniel <845765@qq.com>
2023-09-24 09:13:20 +0800

    :fire: Use `Git LFS` to manage static resource files in `app/stage/protyle/**` https://github.com/siyuan-note/siyuan/issues/9253

--- app/stage/protyle/js/protyle-html.js
+++ app/stage/protyle/js/protyle-html.js
@@ -1,3 +1,43 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:0ce952680d1a8c5ee526d0832cd02dfb845ca3a5b1b47efb560484713c80ad03
-size 1553
+class ProtyleHtml extends HTMLElement {
+  constructor () {
+    super()
+    const shadowRoot = this.attachShadow({mode: 'open'})
+    this.display = this.shadowRoot
+    const dataContent = Lute.UnEscapeHTMLStr(this.getAttribute('data-content'))
+    this.display.innerHTML = dataContent
+  }
+
+  static get observedAttributes () {
+    return ['data-content']
+  }
+
+  attributeChangedCallback (name, oldValue, newValue) {
+    if (name === 'data-content') {
+      const dataContent = Lute.UnEscapeHTMLStr(this.getAttribute('data-content'))
+      this.display.innerHTML = dataContent
+
+      const el = document.createElement('div')
+      el.innerHTML = dataContent
+      const scripts = el.getElementsByTagName('script')
+      let fatalHTML = ''
+      for (const script of scripts) {
+        if (script.textContent.indexOf('document.write') > -1) {
+          fatalHTML += `<div style="color:var(--b3-theme-error);font-size: 12px">${window.siyuan.languages.htmlBlockError}</div>
+<textarea style="width: 100%;box-sizing: border-box;height: 120px"><script>${script.textContent}</script></textarea>`
+        } else {
+          const s = document.createElement('script')
+          for (const attr of script.attributes) {
+            s.setAttribute(attr.name, attr.value);
+          }
+          s.textContent = script.textContent
+          this.display.appendChild(s)
+        }
+      }
+      if (fatalHTML) {
+        this.display.innerHTML += fatalHTML
+      }
+    }
+  }
+}
+
+customElements.define('protyle-html', ProtyleHtml)

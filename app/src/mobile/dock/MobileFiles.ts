56897ad3c8a076997ba505f74ebf9e5d2accba86
Vanessa <lly219@gmail.com>
2023-09-25 16:20:31 +0800

    :art: fix https://github.com/siyuan-note/siyuan/issues/9213

--- app/src/mobile/dock/MobileFiles.ts
+++ app/src/mobile/dock/MobileFiles.ts
@@ -40,8 +40,22 @@ export class MobileFiles extends Model {
                             this.onMount(data);
                             break;
                         case "createnotebook":
-                            setNoteBook();
-                            this.element.insertAdjacentHTML("beforeend", this.genNotebook(data.data.box));
+                            setNoteBook((notebooks) => {
+                                let previousId: string;
+                                notebooks.find(item => {
+                                    if (!item.closed) {
+                                        if (item.id === data.data.box.id) {
+                                            if (previousId) {
+                                                this.element.querySelector(`.b3-list[data-url="${previousId}"]`).insertAdjacentHTML("afterend", this.genNotebook(data.data.box));
+                                            } else {
+                                                this.element.insertAdjacentHTML("afterbegin", this.genNotebook(data.data.box));
+                                            }
+                                            return true;
+                                        }
+                                        previousId = item.id;
+                                    }
+                                });
+                            });
                             break;
                         case "unmount":
                         case "removeDoc":

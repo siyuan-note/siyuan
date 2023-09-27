d3aa7d731f62a327470c2875cffe09a5d72190db
Vanessa <lly219@gmail.com>
2023-09-27 10:51:18 +0800

    :children_crossing: fix https://github.com/siyuan-note/siyuan/issues/9284

--- app/src/search/assets.ts
+++ app/src/search/assets.ts
@@ -63,7 +63,7 @@ export const openSearchAsset = (element: Element, isStick: boolean) => {
     <span class="fn__space"></span>
     <span data-type="assetNext" class="block__icon block__icon--show b3-tooltips b3-tooltips__ne" disabled="disabled" aria-label="${window.siyuan.languages.nextLabel}"><svg><use xlink:href='#iconRight'></use></svg></span>
     <span class="fn__space"></span>
-    <span id="searchAssetResult"></span>
+    <span id="searchAssetResult" class="ft__selectnone"></span>
     <span class="fn__space"></span>
     <span class="fn__flex-1"></span>
 </div>

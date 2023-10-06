dc7f9f6ef64fd21d94533894036e57151a6fcee8
Vanessa <lly219@gmail.com>
2023-10-06 11:06:30 +0800

    :art: database

--- app/src/util/functions.ts
+++ app/src/util/functions.ts
@@ -82,6 +82,7 @@ export const looseJsonParse = (text: string) => {
 
 export const objEquals = (a: any, b: any): boolean => {
     if (a === b) return true;
+    if (typeof a === "number" && isNaN(a) && typeof b === "number" && isNaN(b)) return true;
     if (a instanceof Date && b instanceof Date) return a.getTime() === b.getTime();
     if (!a || !b || (typeof a !== "object" && typeof b !== "object")) return a === b;
     if (a.prototype !== b.prototype) return false;

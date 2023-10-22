221d3c1d7f12b3a3ef4ae4e7b6223784252fc911
Vanessa <lly219@gmail.com>
2023-10-22 20:03:19 +0800

    :loud_sound:

--- app/src/protyle/upload/index.ts
+++ app/src/protyle/upload/index.ts
@@ -252,6 +252,7 @@ export const uploadFiles = (protyle: IProtyle, files: FileList | DataTransferIte
 
     protyle.upload.isUploading = true;
     xhr.onreadystatechange = () => {
+        console.log(xhr.readyState)
         if (xhr.readyState === XMLHttpRequest.DONE) {
             protyle.upload.isUploading = false;
             if (!document.body.contains(protyle.element)) {

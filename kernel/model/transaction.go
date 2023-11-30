d7d4f4d0dee0021428dfd0dfb882f7dd3f9aa932
Daniel <845765@qq.com>
2023-11-30 19:48:30 +0800

    :art: Supports multiple views for the database https://github.com/siyuan-note/siyuan/issues/9751

--- kernel/model/transaction.go
+++ kernel/model/transaction.go
@@ -242,6 +242,10 @@ func performTx(tx *Transaction) (ret *TxErr) {
 			ret = tx.doReplaceAttrViewBlock(op)
 		case "updateAttrViewColTemplate":
 			ret = tx.doUpdateAttrViewColTemplate(op)
+		case "addAttrViewView":
+			ret = tx.doAddAttrViewView(op)
+		case "removeAttrViewView":
+			ret = tx.doRemoveAttrViewView(op)
 		}
 
 		if nil != ret {
@@ -253,6 +257,49 @@ func performTx(tx *Transaction) (ret *TxErr) {
 	if cr := tx.commit(); nil != cr {
 		logging.LogErrorf("commit tx failed: %s", cr)
 		return &TxErr{msg: cr.Error()}
+	}
+	return
+}
+
+func (tx *Transaction) doRemoveAttrViewView(operation *Operation) (ret *TxErr) {
+	var err error
+	avID := operation.AvID
+	attrView, err := av.ParseAttributeView(avID)
+	if nil != err {
+		logging.LogErrorf("parse attribute view [%s] failed: %s", avID, err)
+		return &TxErr{code: TxErrCodeBlockNotFound, id: avID}
+	}
+
+	viewID := operation.ID
+	for i, view := range attrView.Views {
+		if viewID == view.ID {
+			attrView.Views = append(attrView.Views[:i], attrView.Views[i+1:]...)
+			break
+		}
+	}
+
+	if err = av.SaveAttributeView(attrView); nil != err {
+		logging.LogErrorf("save attribute view [%s] failed: %s", avID, err)
+		return &TxErr{code: TxErrCodeWriteTree, msg: err.Error(), id: avID}
+	}
+	return
+}
+
+func (tx *Transaction) doAddAttrViewView(operation *Operation) (ret *TxErr) {
+	var err error
+	avID := operation.AvID
+	attrView, err := av.ParseAttributeView(avID)
+	if nil != err {
+		logging.LogErrorf("parse attribute view [%s] failed: %s", avID, err)
+		return &TxErr{code: TxErrCodeBlockNotFound, id: avID}
+	}
+
+	view := av.NewView()
+	attrView.Views = append(attrView.Views, view)
+	attrView.ViewID = view.ID
+	if err = av.SaveAttributeView(attrView); nil != err {
+		logging.LogErrorf("save attribute view [%s] failed: %s", avID, err)
+		return &TxErr{code: TxErrCodeWriteTree, msg: err.Error(), id: avID}
 	}
 	return
 }

a72d50754c20002316ddcd4e44f73dd87bf946fa
颖逸 <49649786+Zuoqiu-Yingyi@users.noreply.github.com>
2023-12-06 16:13:46 +0800

    :art: Adjust the style of the column pinned to the attribute view

--- app/src/protyle/render/av/render.ts
+++ app/src/protyle/render/av/render.ts
@@ -66,8 +66,8 @@ export const avRender = (element: Element, protyle: IProtyle, cb?: () => void, v
             }, (response) => {
                 const data = response.data.view as IAVTable;
                 // header
-                let tableHTML = '<div class="av__row av__row--header"><div class="av__firstcol av__colsticky"><svg><use xlink:href="#iconUncheck"></use></svg></div>';
-                let calcHTML = '<div style="width: 24px"></div>';
+                let tableHTML = '<div class="av__row av__row--header"><div class="av__colsticky"><div class="av__firstcol"><svg class="av__check"><use xlink:href="#iconUncheck"></use></svg></div>';
+                let calcHTML = '<div class="av__colsticky"><div class="av__firstcol"><svg><use xlink:href="#iconMath"></use></svg></div>';
                 let pinIndex = -1;
                 let pinMaxIndex = -1;
                 let indexWidth = 0;
@@ -84,9 +84,9 @@ export const avRender = (element: Element, protyle: IProtyle, cb?: () => void, v
                     }
                 });
                 pinIndex = Math.min(pinIndex, pinMaxIndex);
-                if (pinIndex > -1) {
-                    tableHTML = '<div class="av__row av__row--header"><div class="av__colsticky"><div class="av__firstcol"><svg><use xlink:href="#iconUncheck"></use></svg></div>';
-                    calcHTML = '<div class="av__colsticky"><div style="width: 24px"></div>';
+                if (pinIndex < 0) {
+                    tableHTML += '</div>';
+                    calcHTML += '</div>';
                 }
                 data.columns.forEach((column: IAVColumn, index: number) => {
                     if (column.hidden) {
@@ -123,11 +123,15 @@ style="width: ${column.width || "200px"}">${getCalcValue(column) || '<svg><use x
 <div class="av__gutters">
     <button class="av__gutter ariaLabel" data-action="add" data-position="right" aria-label="${isMac() ? window.siyuan.languages.addBelowAbove : window.siyuan.languages.addBelowAbove.replace("⌥", "Alt+")}"><svg><use xlink:href="#iconAdd"></use></svg></button>
     <button class="av__gutter ariaLabel" draggable="true" data-position="right" aria-label="${window.siyuan.languages.rowTip}"><svg><use xlink:href="#iconDrag"></use></svg></button>
-</div>`;
-                    if (pinIndex > -1) {
-                        tableHTML += '<div class="av__colsticky"><div class="av__firstcol"><svg><use xlink:href="#iconUncheck"></use></svg></div>';
-                    } else {
-                        tableHTML += "<div class=\"av__firstcol av__colsticky\"><svg><use xlink:href=\"#iconUncheck\"></use></svg></div>";
+</div>
+<div class="av__colsticky">
+    <div class="av__firstcol">
+        <svg class="av__check"><use xlink:href="#iconUncheck"></use></svg>
+    </div>
+`;
+
+                    if (pinIndex < 0) {
+                        tableHTML += '</div>';
                     }
 
                     row.cells.forEach((cell, index) => {
@@ -250,8 +254,12 @@ ${cell.color ? `color:${cell.color};` : ""}">${text}</div>`;
             ${tableHTML}
             <div class="av__row--add">
                 <div class="av__colsticky">
-                    <svg><use xlink:href="#iconAdd"></use></svg>
-                    ${window.siyuan.languages.addAttr}
+                    <div class="av__firstcol">
+                        <svg><use xlink:href="#iconAdd"></use></svg>
+                    </div>
+                    <div class="av__cell--add">
+                        ${window.siyuan.languages.addAttr}
+                    </div>
                 </div>
             </div>
             <div class="av__row--footer">${calcHTML}</div>

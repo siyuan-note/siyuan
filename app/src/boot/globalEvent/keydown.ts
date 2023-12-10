e816e94ba15fc0ee6e172ae3e04162b2240a005e
Vanessa <lly219@gmail.com>
2023-12-10 21:35:35 +0800

    :art: fix https://github.com/siyuan-note/siyuan/issues/9848

--- app/src/boot/globalEvent/keydown.ts
+++ app/src/boot/globalEvent/keydown.ts
@@ -1035,7 +1035,7 @@ const panelTreeKeydown = (app: App, event: KeyboardEvent) => {
 
 let switchDialog: Dialog;
 export const windowKeyDown = (app: App, event: KeyboardEvent) => {
-    if (document.querySelector(".av__mask") || document.getElementById("errorLog") || event.isComposing) {
+    if (document.getElementById("errorLog") || event.isComposing) {
         return;
     }
     const target = event.target as HTMLElement;

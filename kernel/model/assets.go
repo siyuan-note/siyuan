741c2764a4ca48af7871df28d55ba162e0d66a2f
Daniel <845765@qq.com>
2023-12-25 12:35:09 +0800

    :art: `Convert network assets to local` no longer process webpage https://github.com/siyuan-note/siyuan/issues/9965

--- kernel/model/assets.go
+++ kernel/model/assets.go
@@ -326,7 +326,7 @@ func NetAssets2LocalAssets(rootID string) (err error) {
 			request.SetRetryCount(1).SetRetryFixedInterval(3 * time.Second)
 			resp, reqErr := request.Get(u)
 			if strings.Contains(strings.ToLower(resp.GetContentType()), "text/html") {
-				// 忽略超链接网页
+				// 忽略超链接网页 `Convert network assets to local` no longer process webpage https://github.com/siyuan-note/siyuan/issues/9965
 				return ast.WalkContinue
 			}
 

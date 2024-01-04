3abdf1377394fe51d6dd2ac6f820e131635cf3a8
Vanessa <lly219@gmail.com>
2024-01-04 21:04:06 +0800

    :art: fix https://github.com/siyuan-note/siyuan/issues/10067

--- app/src/protyle/render/av/cell.ts
+++ app/src/protyle/render/av/cell.ts
@@ -520,7 +520,7 @@ export const renderCell = (cellValue: IAVCellValue, wrap: boolean) => {
             text = `<span class="av__celltext">${cellValue.block.content || ""}</span>
 <span class="b3-chip b3-chip--info b3-chip--small" data-type="block-more">${window.siyuan.languages.more}</span>`;
         } else {
-            text = `<span data-type="block-ref" data-id="${cellValue.block.id}" data-subtype="s" class="av__celltext av__celltext--ref">${cellValue.block.content || ""}</span>
+            text = `<span data-type="block-ref" data-id="${cellValue.block.id}" data-subtype="s" class="av__celltext av__celltext--ref">${cellValue.block.content || "Untitled"}</span>
 <span class="b3-chip b3-chip--info b3-chip--small popover__block" data-id="${cellValue.block.id}" data-type="block-more">${window.siyuan.languages.update}</span>`;
         }
     } else if (cellValue.type === "number") {

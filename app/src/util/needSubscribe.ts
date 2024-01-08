6f780296515664b553476b766cbc56d74740dd27
EightDoor <851708184@qq.com>
2024-01-08 22:59:10 +0800

    fix: remove isPaidUser return false

--- app/src/util/needSubscribe.ts
+++ app/src/util/needSubscribe.ts
@@ -33,5 +33,6 @@ export const needSubscribe = (tip = window.siyuan.languages._kernel[29]) => {
 };
 
 export const isPaidUser = () => {
-    return window.siyuan.user && (0 === window.siyuan.user.userSiYuanSubscriptionStatus || 1 === window.siyuan.user.userSiYuanOneTimePayStatus);
+    // return window.siyuan.user && (0 === window.siyuan.user.userSiYuanSubscriptionStatus || 1 === window.siyuan.user.userSiYuanOneTimePayStatus);
+    return true;
 };

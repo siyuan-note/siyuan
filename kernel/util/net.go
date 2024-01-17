21462e1947abc607df21de0c160a5593b33cbfef
pinghe <mzhx.meng@gmail.com>
2024-01-17 13:16:05 +0800

    Improved url online checking logic
    Improved url online checking logic. When 401 is returned, it can also be recognized as online

--- kernel/util/net.go
+++ kernel/util/net.go
@@ -100,6 +100,11 @@ func isOnline(checkURL string, skipTlsVerify bool) (ret bool) {
 
 	for i := 0; i < 3; i++ {
 		resp, err := c.R().Get(checkURL)
+
+		if resp.StatusCode == 401 {
+			return true
+		}
+		
 		if resp.GetHeader("Location") != "" {
 			return true
 		}

77db5c6ca2ee741958686b4d2947827065cd8b36
Daniel <845765@qq.com>
2024-01-17 16:51:01 +0800

    :art: Improve data sync cloud storage availability check logic https://github.com/siyuan-note/siyuan/pull/10206

--- kernel/util/net.go
+++ kernel/util/net.go
@@ -100,11 +100,11 @@ func isOnline(checkURL string, skipTlsVerify bool) (ret bool) {
 
 	for i := 0; i < 3; i++ {
 		resp, err := c.R().Get(checkURL)
-
-		if resp.StatusCode == 401 {
+		if 401 == resp.StatusCode {
+			// Improve data sync cloud storage availability check logic https://github.com/siyuan-note/siyuan/pull/10206
 			return true
 		}
-		
+
 		if resp.GetHeader("Location") != "" {
 			return true
 		}

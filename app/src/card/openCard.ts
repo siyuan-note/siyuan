9b815d9613b14cebdf4b5d4efee81c4b018a7066
Vanessa <lly219@gmail.com>
2024-03-17 23:21:39 +0800

    :art: card

--- app/src/card/openCard.ts
+++ app/src/card/openCard.ts
@@ -389,10 +389,6 @@ export const bindCardEvent = async (options: {
     <div class="fn__flex-1">${window.siyuan.languages.forgetCount}</div>
     <div class="fn__space"></div>
     <div>${currentCard.lapses}</div>
-</div><div class="fn__flex${currentCard.lastReview > 0 ? "" : " fn__none"}">
-    <div class="fn__flex-1">${window.siyuan.languages.lastReviewTime}</div>
-    <div class="fn__space"></div>
-    <div>${dayjs(currentCard.lastReview).format("YYYY-MM-DD")}</div>
 </div><div class="fn__flex">
     <div class="fn__flex-1">${window.siyuan.languages.revisionCount}</div>
     <div class="fn__space"></div>
@@ -401,6 +397,10 @@ export const bindCardEvent = async (options: {
     <div class="fn__flex-1">${window.siyuan.languages.cardStatus}</div>
     <div class="fn__space"></div>
     <div class="${currentCard.state === 0 ? "ft__primary" : "ft__success"}">${currentCard.state === 0 ? window.siyuan.languages.flashcardNewCard : window.siyuan.languages.flashcardReviewCard}</div>
+</div><div class="fn__flex${currentCard.lastReview > 0 ? "" : " fn__none"}">
+    <div class="fn__flex-1">${window.siyuan.languages.lastReviewTime}</div>
+    <div class="fn__space"></div>
+    <div>${dayjs(currentCard.lastReview).format("YYYY-MM-DD")}</div>
 </div>`,
                 });
                 /// #if MOBILE

b259d1699a59326d61754e0b504c231be307ed9f
Vanessa <lly219@gmail.com>
2024-03-17 23:13:45 +0800

    :bug: fix https://github.com/siyuan-note/siyuan/issues/10633

--- app/src/protyle/wysiwyg/enter.ts
+++ app/src/protyle/wysiwyg/enter.ts
@@ -17,195 +17,6 @@ import {isIPad, setStorageVal} from "../util/compatibility";
 import {mathRender} from "../render/mathRender";
 import {isMobile} from "../../util/functions";
 
-const listEnter = (protyle: IProtyle, blockElement: HTMLElement, range: Range) => {
-    const listItemElement = blockElement.parentElement;
-
-    const editableElement = getContenteditableElement(blockElement);
-    if (// \n 是因为 https://github.com/siyuan-note/siyuan/issues/3846
-        ["", "\n"].includes(editableElement.textContent) &&
-        blockElement.previousElementSibling.classList.contains("protyle-action") &&
-        !blockElement.querySelector("img") // https://ld246.com/article/1651820644238
-    ) {
-        if (listItemElement.nextElementSibling?.classList.contains("protyle-attr")) {
-            listOutdent(protyle, [blockElement.parentElement], range);
-            return true;
-        } else if (!listItemElement.parentElement.classList.contains("protyle-wysiwyg")) {
-            // 打断列表
-            breakList(protyle, blockElement, range);
-            return true;
-        }
-    }
-
-    const position = getSelectionOffset(editableElement, protyle.wysiwyg.element, range);
-    if (range.toString() === "" && position.start === 0 &&
-        // 段首为图片是 start 也为 0
-        !hasPreviousSibling(range.startContainer)) {
-        // 段首换行
-        if (listItemElement.parentElement.classList.contains("protyle-wysiwyg")) {
-            return true;
-        }
-        // https://github.com/siyuan-note/siyuan/issues/8935
-        const wbrElement = document.createElement("wbr");
-        range.insertNode(wbrElement);
-        const html = listItemElement.parentElement.outerHTML;
-        wbrElement.remove();
-        let newElement = genListItemElement(listItemElement, -1, true);
-        if (!blockElement.previousElementSibling.classList.contains("protyle-action")) {
-            // 列表项中有多个块，最后一个块为空，换行应进行缩进
-            if (getContenteditableElement(blockElement).textContent !== "") {
-                return false;
-            }
-            blockElement.remove();
-            newElement = genListItemElement(listItemElement, -1, true);
-            listItemElement.insertAdjacentElement("afterend", newElement);
-        } else if (getContenteditableElement(blockElement).textContent === "") {
-            listItemElement.insertAdjacentElement("afterend", newElement);
-        } else {
-            listItemElement.insertAdjacentElement("beforebegin", newElement);
-        }
-        if (listItemElement.getAttribute("data-subtype") === "o") {
-            updateListOrder(listItemElement.parentElement);
-        }
-        updateTransaction(protyle, listItemElement.parentElement.getAttribute("data-node-id"), listItemElement.parentElement.outerHTML, html);
-        focusByWbr(newElement, range);
-        scrollCenter(protyle);
-        removeEmptyNode(newElement);
-        return true;
-    }
-
-    const subListElement = listItemElement.querySelector(".list");
-    let newElement;
-    if (subListElement && listItemElement.getAttribute("fold") !== "1" &&
-        // 子列表下的段落块回车 https://ld246.com/article/1623919354587
-        blockElement.nextElementSibling.isSameNode(subListElement)) {
-        // 含有子列表的换行
-        if (position.end >= editableElement.textContent.length -
-            // 数学公式结尾会有 zwsp https://github.com/siyuan-note/siyuan/issues/6679
-            (editableElement.textContent.endsWith(Constants.ZWSP) ? 1 : 0)) {
-            // 段末换行，在子列表中插入
-            range.insertNode(document.createElement("wbr"));
-            const html = subListElement.outerHTML;
-            blockElement.querySelector("wbr").remove();
-            newElement = genListItemElement(subListElement.firstElementChild, -1, true);
-            subListElement.firstElementChild.before(newElement);
-            if (subListElement.getAttribute("data-subtype") === "o") {
-                updateListOrder(subListElement);
-            }
-            updateTransaction(protyle, subListElement.getAttribute("data-node-id"), subListElement.outerHTML, html);
-            focusByWbr(listItemElement, range);
-            scrollCenter(protyle);
-        } else {
-            // 文字中间换行
-            range.insertNode(document.createElement("wbr"));
-            const listItemHTMl = listItemElement.outerHTML;
-            const html = listItemElement.parentElement.outerHTML;
-            if (range.toString() !== "") {
-                range.extractContents();
-                range.insertNode(document.createElement("wbr"));
-            }
-            range.setEndAfter(editableElement.lastChild);
-            newElement = genListItemElement(listItemElement, 0, false);
-            const newEditElement = getContenteditableElement(newElement);
-            newEditElement.appendChild(range.extractContents());
-            newEditElement.parentElement.after(subListElement);
-            listItemElement.insertAdjacentElement("afterend", newElement);
-            if (listItemElement.getAttribute("data-subtype") === "o") {
-                updateListOrder(listItemElement.parentElement);
-            }
-            if (listItemElement.parentElement.classList.contains("protyle-wysiwyg")) {
-                transaction(protyle, [{
-                    action: "update",
-                    data: listItemElement.outerHTML,
-                    id: listItemElement.getAttribute("data-node-id")
-                }, {
-                    action: "insert",
-                    id: newElement.getAttribute("data-node-id"),
-                    data: newElement.outerHTML,
-                    previousID: listItemElement.getAttribute("data-node-id")
-                }], [{
-                    action: "delete",
-                    id: newElement.getAttribute("data-node-id"),
-                }, {
-                    action: "update",
-                    data: listItemHTMl,
-                    id: listItemElement.getAttribute("data-node-id")
-                }]);
-            } else {
-                updateTransaction(protyle, listItemElement.parentElement.getAttribute("data-node-id"), listItemElement.parentElement.outerHTML, html);
-            }
-            focusByWbr(newElement, range);
-            scrollCenter(protyle);
-        }
-        removeEmptyNode(newElement);
-        return true;
-    }
-    if ((range.toString() === "" || range.toString() === Constants.ZWSP) && range.startContainer.nodeType === 3 && range.startOffset === 0) {
-        // 图片后的零宽空格前回车 https://github.com/siyuan-note/siyuan/issues/5690
-        // 列表中的图片后双击换行图片光标错误 https://ld246.com/article/1660987186727/comment/1662181221732?r=Vanessa#comments
-        let nextSibling = range.startContainer;
-        while (nextSibling) {
-            if (nextSibling.textContent === Constants.ZWSP) {
-                range.setStart(nextSibling, 1);
-                range.collapse(false);
-                break;
-            } else {
-                nextSibling = nextSibling.nextSibling;
-            }
-        }
-    }
-    range.insertNode(document.createElement("wbr"));
-    const listItemHTML = listItemElement.outerHTML;
-    const html = listItemElement.parentElement.outerHTML;
-    if (range.toString() !== "") {
-        range.extractContents();
-        range.insertNode(document.createElement("wbr"));
-    }
-    range.setEndAfter(editableElement.lastChild);
-    newElement = genListItemElement(listItemElement, 0, false);
-    const selectNode = range.extractContents();
-    if (selectNode.firstChild.nodeType !== 3 && selectNode.firstChild.textContent === "") {
-        // 回车移除空元素 https://github.com/siyuan-note/insider/issues/480
-        selectNode.firstChild.after(document.createElement("wbr"));
-        selectNode.firstChild.remove();
-    }
-    // https://github.com/siyuan-note/siyuan/issues/3850
-    // https://github.com/siyuan-note/siyuan/issues/6018
-    if ((editableElement?.lastElementChild?.getAttribute("data-type") || "").indexOf("inline-math") > -1 &&
-        !hasNextSibling(editableElement?.lastElementChild)) {
-        editableElement.insertAdjacentText("beforeend", "\n");
-    }
-    getContenteditableElement(newElement).appendChild(selectNode);
-    listItemElement.insertAdjacentElement("afterend", newElement);
-    if (listItemElement.getAttribute("data-subtype") === "o") {
-        updateListOrder(listItemElement.parentElement);
-    }
-    if (listItemElement.parentElement.classList.contains("protyle-wysiwyg")) {
-        transaction(protyle, [{
-            action: "update",
-            id: listItemElement.getAttribute("data-node-id"),
-            data: listItemElement.outerHTML,
-        }, {
-            action: "insert",
-            id: newElement.getAttribute("data-node-id"),
-            data: newElement.outerHTML,
-            previousID: listItemElement.getAttribute("data-node-id")
-        }], [{
-            action: "delete",
-            id: newElement.getAttribute("data-node-id"),
-        }, {
-            action: "update",
-            id: listItemElement.getAttribute("data-node-id"),
-            data: listItemHTML
-        }]);
-    } else {
-        updateTransaction(protyle, listItemElement.parentElement.getAttribute("data-node-id"), listItemElement.parentElement.outerHTML, html);
-    }
-    focusByWbr(newElement, range);
-    scrollCenter(protyle);
-    removeEmptyNode(newElement);
-    return true;
-};
-
 export const enter = (blockElement: HTMLElement, range: Range, protyle: IProtyle) => {
     const disableElement = isNotEditBlock(blockElement);
     if (!disableElement && blockElement.classList.contains("protyle-wysiwyg--select")) {
@@ -215,7 +26,9 @@ export const enter = (blockElement: HTMLElement, range: Range, protyle: IProtyle
         return;
     }
     // https://github.com/siyuan-note/siyuan/issues/5471
-    if (disableElement) {
+    if (disableElement ||
+        // https://github.com/siyuan-note/siyuan/issues/10633
+        blockElement.classList.contains("table")) {
         if (blockElement.parentElement.classList.contains("li")) {
             const oldHTML = blockElement.parentElement.parentElement.outerHTML;
             const newElement = genListItemElement(blockElement.parentElement, 0, true);
@@ -443,6 +256,195 @@ export const enter = (blockElement: HTMLElement, range: Range, protyle: IProtyle
     transaction(protyle, doOperation, undoOperation);
     focusBlock(blockElement.nextElementSibling);
     scrollCenter(protyle);
+    return true;
+};
+
+const listEnter = (protyle: IProtyle, blockElement: HTMLElement, range: Range) => {
+    const listItemElement = blockElement.parentElement;
+
+    const editableElement = getContenteditableElement(blockElement);
+    if (// \n 是因为 https://github.com/siyuan-note/siyuan/issues/3846
+        ["", "\n"].includes(editableElement.textContent) &&
+        blockElement.previousElementSibling.classList.contains("protyle-action") &&
+        !blockElement.querySelector("img") // https://ld246.com/article/1651820644238
+    ) {
+        if (listItemElement.nextElementSibling?.classList.contains("protyle-attr")) {
+            listOutdent(protyle, [blockElement.parentElement], range);
+            return true;
+        } else if (!listItemElement.parentElement.classList.contains("protyle-wysiwyg")) {
+            // 打断列表
+            breakList(protyle, blockElement, range);
+            return true;
+        }
+    }
+
+    const position = getSelectionOffset(editableElement, protyle.wysiwyg.element, range);
+    if (range.toString() === "" && position.start === 0 &&
+        // 段首为图片是 start 也为 0
+        !hasPreviousSibling(range.startContainer)) {
+        // 段首换行
+        if (listItemElement.parentElement.classList.contains("protyle-wysiwyg")) {
+            return true;
+        }
+        // https://github.com/siyuan-note/siyuan/issues/8935
+        const wbrElement = document.createElement("wbr");
+        range.insertNode(wbrElement);
+        const html = listItemElement.parentElement.outerHTML;
+        wbrElement.remove();
+        let newElement = genListItemElement(listItemElement, -1, true);
+        if (!blockElement.previousElementSibling.classList.contains("protyle-action")) {
+            // 列表项中有多个块，最后一个块为空，换行应进行缩进
+            if (getContenteditableElement(blockElement).textContent !== "") {
+                return false;
+            }
+            blockElement.remove();
+            newElement = genListItemElement(listItemElement, -1, true);
+            listItemElement.insertAdjacentElement("afterend", newElement);
+        } else if (getContenteditableElement(blockElement).textContent === "") {
+            listItemElement.insertAdjacentElement("afterend", newElement);
+        } else {
+            listItemElement.insertAdjacentElement("beforebegin", newElement);
+        }
+        if (listItemElement.getAttribute("data-subtype") === "o") {
+            updateListOrder(listItemElement.parentElement);
+        }
+        updateTransaction(protyle, listItemElement.parentElement.getAttribute("data-node-id"), listItemElement.parentElement.outerHTML, html);
+        focusByWbr(newElement, range);
+        scrollCenter(protyle);
+        removeEmptyNode(newElement);
+        return true;
+    }
+
+    const subListElement = listItemElement.querySelector(".list");
+    let newElement;
+    if (subListElement && listItemElement.getAttribute("fold") !== "1" &&
+        // 子列表下的段落块回车 https://ld246.com/article/1623919354587
+        blockElement.nextElementSibling.isSameNode(subListElement)) {
+        // 含有子列表的换行
+        if (position.end >= editableElement.textContent.length -
+            // 数学公式结尾会有 zwsp https://github.com/siyuan-note/siyuan/issues/6679
+            (editableElement.textContent.endsWith(Constants.ZWSP) ? 1 : 0)) {
+            // 段末换行，在子列表中插入
+            range.insertNode(document.createElement("wbr"));
+            const html = subListElement.outerHTML;
+            blockElement.querySelector("wbr").remove();
+            newElement = genListItemElement(subListElement.firstElementChild, -1, true);
+            subListElement.firstElementChild.before(newElement);
+            if (subListElement.getAttribute("data-subtype") === "o") {
+                updateListOrder(subListElement);
+            }
+            updateTransaction(protyle, subListElement.getAttribute("data-node-id"), subListElement.outerHTML, html);
+            focusByWbr(listItemElement, range);
+            scrollCenter(protyle);
+        } else {
+            // 文字中间换行
+            range.insertNode(document.createElement("wbr"));
+            const listItemHTMl = listItemElement.outerHTML;
+            const html = listItemElement.parentElement.outerHTML;
+            if (range.toString() !== "") {
+                range.extractContents();
+                range.insertNode(document.createElement("wbr"));
+            }
+            range.setEndAfter(editableElement.lastChild);
+            newElement = genListItemElement(listItemElement, 0, false);
+            const newEditElement = getContenteditableElement(newElement);
+            newEditElement.appendChild(range.extractContents());
+            newEditElement.parentElement.after(subListElement);
+            listItemElement.insertAdjacentElement("afterend", newElement);
+            if (listItemElement.getAttribute("data-subtype") === "o") {
+                updateListOrder(listItemElement.parentElement);
+            }
+            if (listItemElement.parentElement.classList.contains("protyle-wysiwyg")) {
+                transaction(protyle, [{
+                    action: "update",
+                    data: listItemElement.outerHTML,
+                    id: listItemElement.getAttribute("data-node-id")
+                }, {
+                    action: "insert",
+                    id: newElement.getAttribute("data-node-id"),
+                    data: newElement.outerHTML,
+                    previousID: listItemElement.getAttribute("data-node-id")
+                }], [{
+                    action: "delete",
+                    id: newElement.getAttribute("data-node-id"),
+                }, {
+                    action: "update",
+                    data: listItemHTMl,
+                    id: listItemElement.getAttribute("data-node-id")
+                }]);
+            } else {
+                updateTransaction(protyle, listItemElement.parentElement.getAttribute("data-node-id"), listItemElement.parentElement.outerHTML, html);
+            }
+            focusByWbr(newElement, range);
+            scrollCenter(protyle);
+        }
+        removeEmptyNode(newElement);
+        return true;
+    }
+    if ((range.toString() === "" || range.toString() === Constants.ZWSP) && range.startContainer.nodeType === 3 && range.startOffset === 0) {
+        // 图片后的零宽空格前回车 https://github.com/siyuan-note/siyuan/issues/5690
+        // 列表中的图片后双击换行图片光标错误 https://ld246.com/article/1660987186727/comment/1662181221732?r=Vanessa#comments
+        let nextSibling = range.startContainer;
+        while (nextSibling) {
+            if (nextSibling.textContent === Constants.ZWSP) {
+                range.setStart(nextSibling, 1);
+                range.collapse(false);
+                break;
+            } else {
+                nextSibling = nextSibling.nextSibling;
+            }
+        }
+    }
+    range.insertNode(document.createElement("wbr"));
+    const listItemHTML = listItemElement.outerHTML;
+    const html = listItemElement.parentElement.outerHTML;
+    if (range.toString() !== "") {
+        range.extractContents();
+        range.insertNode(document.createElement("wbr"));
+    }
+    range.setEndAfter(editableElement.lastChild);
+    newElement = genListItemElement(listItemElement, 0, false);
+    const selectNode = range.extractContents();
+    if (selectNode.firstChild.nodeType !== 3 && selectNode.firstChild.textContent === "") {
+        // 回车移除空元素 https://github.com/siyuan-note/insider/issues/480
+        selectNode.firstChild.after(document.createElement("wbr"));
+        selectNode.firstChild.remove();
+    }
+    // https://github.com/siyuan-note/siyuan/issues/3850
+    // https://github.com/siyuan-note/siyuan/issues/6018
+    if ((editableElement?.lastElementChild?.getAttribute("data-type") || "").indexOf("inline-math") > -1 &&
+        !hasNextSibling(editableElement?.lastElementChild)) {
+        editableElement.insertAdjacentText("beforeend", "\n");
+    }
+    getContenteditableElement(newElement).appendChild(selectNode);
+    listItemElement.insertAdjacentElement("afterend", newElement);
+    if (listItemElement.getAttribute("data-subtype") === "o") {
+        updateListOrder(listItemElement.parentElement);
+    }
+    if (listItemElement.parentElement.classList.contains("protyle-wysiwyg")) {
+        transaction(protyle, [{
+            action: "update",
+            id: listItemElement.getAttribute("data-node-id"),
+            data: listItemElement.outerHTML,
+        }, {
+            action: "insert",
+            id: newElement.getAttribute("data-node-id"),
+            data: newElement.outerHTML,
+            previousID: listItemElement.getAttribute("data-node-id")
+        }], [{
+            action: "delete",
+            id: newElement.getAttribute("data-node-id"),
+        }, {
+            action: "update",
+            id: listItemElement.getAttribute("data-node-id"),
+            data: listItemHTML
+        }]);
+    } else {
+        updateTransaction(protyle, listItemElement.parentElement.getAttribute("data-node-id"), listItemElement.parentElement.outerHTML, html);
+    }
+    focusByWbr(newElement, range);
+    scrollCenter(protyle);
+    removeEmptyNode(newElement);
     return true;
 };
 

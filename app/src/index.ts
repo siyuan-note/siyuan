e0adb2f60c421adbfb9fc7daacad943cc3129dc4
Vanessa <lly219@gmail.com>
2024-03-29 20:50:04 +0800

    :art: https://github.com/siyuan-note/siyuan/issues/7957

--- app/src/index.ts
+++ app/src/index.ts
@@ -65,6 +65,9 @@ export class App {
                             case "syncMergeResult":
                                 reloadSync(this, data.data);
                                 break;
+                            case "reloaddoc":
+                                reloadSync(this, {upsertRootIDs: [data.data], removeRootIDs: []});
+                                break;
                             case "readonly":
                                 window.siyuan.config.editor.readOnly = data.data;
                                 hideAllElements(["util"]);

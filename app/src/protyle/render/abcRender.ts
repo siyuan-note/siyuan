b5655da1212ee7094ace58e575e147b13f8aa3bf
Clegs <2176663+Clegs@users.noreply.github.com>
2024-04-26 13:41:17 -0700

    Allow users to specify abcjsParams

--- app/src/protyle/render/abcRender.ts
+++ app/src/protyle/render/abcRender.ts
@@ -3,6 +3,30 @@ import {Constants} from "../../constants";
 import {genIconHTML} from "./util";
 import {hasClosestByClassName} from "../util/hasClosest";
 
+const ABCJS_PARAMS_KEY = "%%abcjsParams:";
+
+// Read the abcjsParams from the content if it exists.
+// The params *must* be the first line of the content in the form:
+// %%abcjsParams: {JSON}
+const getAbcParams = (abcString: string): any => {
+    let params = {
+        responsive: 'resize',
+    };
+
+    const firstLine = abcString.substring(0, abcString.indexOf("\n"));
+
+    if (firstLine.startsWith(ABCJS_PARAMS_KEY)) {
+        try {
+            const jsonString = firstLine.substring(ABCJS_PARAMS_KEY.length);
+            params = { ...params, ...JSON.parse(jsonString) };
+        } catch (e) {
+            console.error(`Failed to parse ABCJS params: ${e}`);
+        }
+    }
+
+    return params;
+};
+
 export const abcRender = (element: Element, cdn = Constants.PROTYLE_CDN) => {
     let abcElements: Element[] = [];
     if (element.getAttribute("data-subtype") === "abc") {
@@ -26,9 +50,9 @@ export const abcRender = (element: Element, cdn = Constants.PROTYLE_CDN) => {
                 }
                 const renderElement = e.firstElementChild.nextElementSibling as HTMLElement;
                 renderElement.innerHTML = `<span style="position: absolute;left:0;top:0;width: 1px;">${Constants.ZWSP}</span><div contenteditable="false"></div>`;
-                window.ABCJS.renderAbc(renderElement.lastElementChild, Lute.UnEscapeHTMLStr(e.getAttribute("data-content")), {
-                    responsive: "resize"
-                });
+                const abcString = Lute.UnEscapeHTMLStr(e.getAttribute("data-content"));
+                const abcjsParams = getAbcParams(abcString)
+                window.ABCJS.renderAbc(renderElement.lastElementChild, abcString, abcjsParams);
                 e.setAttribute("data-render", "true");
             });
         });

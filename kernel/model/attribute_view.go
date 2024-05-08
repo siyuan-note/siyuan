83209e6ab2156effd71ab14f3369ebacb38fabf2
Daniel <845765@qq.com>
2024-05-08 19:52:10 +0800

    :art: Improve database text, email, url and phone field content escaping https://github.com/siyuan-note/siyuan/issues/11255

--- kernel/model/attribute_view.go
+++ kernel/model/attribute_view.go
@@ -1087,9 +1087,21 @@ func renderAttributeViewTable(attrView *av.AttributeView, view *av.View, query s
 				if nil != tableCell.Value && nil != tableCell.Value.Relation {
 					tableCell.Value.Relation.Contents = nil
 				}
-			case av.KeyTypeText: // 渲染文本列
+			case av.KeyTypeText:
 				if nil != tableCell.Value && nil != tableCell.Value.Text {
 					tableCell.Value.Text.Content = util.EscapeHTML(tableCell.Value.Text.Content)
+				}
+			case av.KeyTypeEmail:
+				if nil != tableCell.Value && nil != tableCell.Value.Email {
+					tableCell.Value.Email.Content = util.EscapeHTML(tableCell.Value.Email.Content)
+				}
+			case av.KeyTypeURL:
+				if nil != tableCell.Value && nil != tableCell.Value.URL {
+					tableCell.Value.URL.Content = util.EscapeHTML(tableCell.Value.URL.Content)
+				}
+			case av.KeyTypePhone:
+				if nil != tableCell.Value && nil != tableCell.Value.Phone {
+					tableCell.Value.Phone.Content = util.EscapeHTML(tableCell.Value.Phone.Content)
 				}
 			}
 

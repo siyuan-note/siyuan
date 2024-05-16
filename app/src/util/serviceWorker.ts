e33acd5765d4e65c24ebfe2f58295d45e92202ca
颖逸 <49649786+Zuoqiu-Yingyi@users.noreply.github.com>
2024-05-17 02:01:28 +0800

    :bug: Fixed the issue that PWA-related files could not pass BasicAuth

--- app/src/util/serviceWorker.ts
+++ app/src/util/serviceWorker.ts
@@ -1,17 +1,21 @@
 // https://github.com/siyuan-note/siyuan/pull/8012
-export const registerServiceWorker = (scriptURL: string, scope = "/", workerType: WorkerType = "module") => {
+export const registerServiceWorker = (
+    scriptURL: string,
+    options: RegistrationOptions = {
+        scope: "/",
+        type: "classic",
+        updateViaCache: "all",
+    }) => {
     if (!("serviceWorker" in navigator) || typeof (navigator.serviceWorker) === "undefined" ||
         !("caches" in window) || !("fetch" in window)) {
         return;
     }
     // REF https://developer.mozilla.org/en-US/docs/Web/API/ServiceWorkerRegistration
     window.navigator.serviceWorker
-        .register(scriptURL, {
-            scope,
-            type: workerType,
-        }).then(registration => {
-        registration.update();
-    }).catch(e => {
-        console.debug(`Registration failed with ${e}`);
-    });
+        .register(scriptURL, options)
+        .then(registration => {
+            registration.update();
+        }).catch(e => {
+            console.debug(`Registration failed with ${e}`);
+        });
 };

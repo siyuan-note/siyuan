5cf02f67d91050562a7e30d5e7e94dea365e7695
Daniel <845765@qq.com>
2024-05-28 23:40:32 +0800

    :art: Support Linux arm64 https://github.com/siyuan-note/siyuan/issues/11566

--- scripts/linux-build.sh
+++ scripts/linux-build.sh
@@ -8,6 +8,7 @@ cd ..
 echo 'Cleaning Builds'
 rm -rf app/build
 rm -rf app/kernel-linux
+rm -rf app/kernel-linux-arm64
 
 echo 'Building Kernel'
 
@@ -20,6 +21,10 @@ export CGO_ENABLED=1
 export GOOS=linux
 export GOARCH=amd64
 go build --tags fts5 -v -o "../app/kernel-linux/SiYuan-Kernel" -ldflags "-s -w" .
+
+export GOARCH=arm64
+export CC=~/aarch64-linux-musl-cross/bin/aarch64-linux-musl-gcc
+go build --tags fts5 -v -o "../app/kernel-linux/SiYuan-Kernel-arm64" -ldflags "-s -w" .
 cd ..
 
 echo 'Building Electron App'

ee115ab3915a4de05a27ec30517837334d61615b
Daniel <845765@qq.com>
2024-06-03 17:06:38 +0800

    :art: Add deb release package for Linux https://github.com/siyuan-note/siyuan/issues/11532

--- scripts/linux-build.sh
+++ scripts/linux-build.sh
@@ -21,6 +21,7 @@ export CGO_ENABLED=1
 echo 'Building Kernel amd64'
 export GOOS=linux
 export GOARCH=amd64
+export CC=~/x86_64-linux-musl-cross/bin/x86_64-linux-musl-gcc
 go build -buildmode=pie --tags fts5 -v -o "../app/kernel-linux/SiYuan-Kernel" -ldflags "-s -w -extldflags -static-pie" .
 
 echo 'Building Kernel arm64'

1d5a486d57fbf479f72aeb4135d094060680efd8
Daniel <845765@qq.com>
2024-06-11 17:40:33 +0800

    :art: Remove invisible characters from file names when uploading assets https://github.com/siyuan-note/siyuan/issues/11683

--- kernel/util/file.go
+++ kernel/util/file.go
@@ -242,6 +242,7 @@ func FilterFileName(name string) string {
 	name = strings.ReplaceAll(name, ">", "")
 	name = strings.ReplaceAll(name, "|", "")
 	name = strings.TrimSpace(name)
+	name = gulu.Str.RemoveInvisible(name) // Remove invisible characters from file names when uploading assets https://github.com/siyuan-note/siyuan/issues/11683
 	return name
 }
 

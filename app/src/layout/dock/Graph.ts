c5b323c3918038510cff27cc9966c1e656c7cdbf
Vanessa <lly219@gmail.com>
2024-06-23 12:52:15 +0800

    :art: fix https://github.com/siyuan-note/siyuan/issues/11774

--- app/src/layout/dock/Graph.ts
+++ app/src/layout/dock/Graph.ts
@@ -322,6 +322,12 @@ export class Graph extends Model {
                         this.searchGraph(false, undefined, true);
                     } else if (dataType === "fullscreen") {
                         fullscreen(this.element, target);
+                        const minElement = this.element.querySelector('.block__icons .block__icon[data-type="min"]')
+                        if (this.element.className.includes("fullscreen")) {
+                            minElement.classList.add("fn__none")
+                        } else {
+                            minElement.classList.remove("fn__none")
+                        }
                     }
                     break;
                 } else if (target.classList.contains("graph__svg")) {

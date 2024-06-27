0859df6ca0de7e4440113db9a185a0e0ab899371
Daniel <845765@qq.com>
2024-06-27 21:42:54 +0800

    :art: Improve data sync when exiting https://github.com/siyuan-note/siyuan/issues/11847

--- kernel/model/sync.go
+++ kernel/model/sync.go
@@ -259,12 +259,6 @@ func checkSync(boot, exit, byHand bool) bool {
 		}
 	}
 
-	if isSyncing.Load() {
-		logging.LogWarnf("sync is in progress")
-		planSyncAfter(fixSyncInterval)
-		return false
-	}
-
 	if 7 < autoSyncErrCount && !byHand {
 		logging.LogErrorf("failed to auto-sync too many times, delay auto-sync 64 minutes")
 		util.PushErrMsg(Conf.Language(125), 1000*60*60)

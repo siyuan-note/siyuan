b351c39514893de011f7cc3a6c51898d45149e7c
Vanessa <lly219@gmail.com>
2024-07-04 16:54:59 +0800

    :art: fix https://github.com/siyuan-note/siyuan/issues/11870

--- app/src/protyle/wysiwyg/remove.ts
+++ app/src/protyle/wysiwyg/remove.ts
@@ -239,7 +239,8 @@ export const removeBlock = (protyle: IProtyle, blockElement: Element, range: Ran
         return;
     }
 
-    if (blockElement.parentElement.classList.contains("li") && blockElement.previousElementSibling.classList.contains("protyle-action")) {
+    if (blockElement.parentElement.classList.contains("li") && blockElement.getAttribute("data-type") !== "NodeHeading" &&
+        blockElement.previousElementSibling.classList.contains("protyle-action")) {
         removeLi(protyle, blockElement, range, type === "Delete");
         return;
     }

dd3aef1f9014f53c01a8f4d3679ce9b947679a5c
Vanessa <lly219@gmail.com>
2024-09-22 00:29:52 +0800

    :art: https://github.com/siyuan-note/siyuan/issues/11587

--- app/src/layout/dock/Files.ts
+++ app/src/layout/dock/Files.ts
@@ -20,7 +20,7 @@ import {hasClosestByAttribute, hasClosestByTag, hasTopClosestByTag} from "../../
 import {isTouchDevice} from "../../util/functions";
 import {App} from "../../index";
 import {refreshFileTree} from "../../dialog/processSystem";
-import {hideTooltip} from "../../dialog/tooltip";
+import {hideTooltip, showTooltip} from "../../dialog/tooltip";
 
 export class Files extends Model {
     public element: HTMLElement;
@@ -422,8 +422,13 @@ export class Files extends Model {
             }
         });
         this.element.addEventListener("dragend", () => {
-            this.element.querySelectorAll(".b3-list-item--focus").forEach((item: HTMLElement) => {
+            this.element.querySelectorAll(".b3-list-item--focus").forEach((item: HTMLElement, index) => {
                 item.style.opacity = "";
+                // https://github.com/siyuan-note/siyuan/issues/11587
+                if (index === 0) {
+                    const airaLabelElement = item.querySelector(".ariaLabel")
+                    showTooltip(airaLabelElement.getAttribute("aria-label"), airaLabelElement);
+                }
             });
             window.siyuan.dragElement = undefined;
         });

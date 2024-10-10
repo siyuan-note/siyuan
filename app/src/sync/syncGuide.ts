dbee0892ba53bc200f63cc84ae248ee8804349f5
Daniel <845765@qq.com>
2024-10-11 01:35:07 +0800

    :art: Improve text

--- app/src/sync/syncGuide.ts
+++ app/src/sync/syncGuide.ts
@@ -55,7 +55,7 @@ export const bindSyncCloudListEvent = (cloudPanelElement: Element, cb?: () => vo
                         addCloudName(cloudPanelElement);
                         break;
                     case "removeCloud":
-                        confirmDialog(window.siyuan.languages.confirm, `${window.siyuan.languages.confirmDeleteCloudDir} <i>${target.parentElement.getAttribute("data-name")}</i>`, () => {
+                        confirmDialog(window.siyuan.languages.deleteOpConfirm, `${window.siyuan.languages.confirmDeleteCloudDir} <i>${target.parentElement.getAttribute("data-name")}</i>`, () => {
                             cloudPanelElement.innerHTML = '<img style="margin: 0 auto;display: block;width: 64px;height: 100%" src="/stage/loading-pure.svg">';
                             fetchPost("/api/sync/removeCloudSyncDir", {name: target.parentElement.getAttribute("data-name")}, (response) => {
                                 window.siyuan.config.sync.cloudName = response.data;

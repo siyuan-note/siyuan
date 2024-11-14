e164e88dc44b36473ab8902585793e4d8dd7b88b
Jeffrey Chen <78434827+TCOTC@users.noreply.github.com>
2024-11-15 00:00:37 +0800

    :art: Modify and standardize database terminology fix #12935 (#13152)
    * 中文 i18n
    
    * 简体中文 繁体中文 用户指南
    
    * 繁体中文 i18n
    
    * 英文 用户指南
    
    * 英文 i18n
    
    * 数据库字段菜单
    
    * 数据库视图菜单
    
    * 数据库条目右键菜单
    
    分隔线关联：
    https://github.com/siyuan-note/siyuan/issues/12518
    https://github.com/siyuan-note/siyuan/issues/12910
    
    * 顺序调整
    
    * 简体中文 繁体中文 i18n

--- app/src/protyle/render/av/openMenuPanel.ts
+++ app/src/protyle/render/av/openMenuPanel.ts
@@ -1372,7 +1372,7 @@ ${hideHTML}`;
     <span class="block__icon" style="padding: 8px;margin-left: -4px;" data-type="go-config">
         <svg><use xlink:href="#iconLeft"></use></svg>
     </span>
-    <span class="b3-menu__label ft__center">${window.siyuan.languages.attr}</span>
+    <span class="b3-menu__label ft__center">${window.siyuan.languages.fields}</span>
 </button>
 <button class="b3-menu__separator"></button>
 <button class="b3-menu__item" data-type="nobg">

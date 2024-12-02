1036bc3f4a069c1abb3afbc00cd05441adb5dad9
Daniel <845765@qq.com>
2024-12-02 20:28:14 +0800

    :art: Support HarmonyOS NEXT system

--- kernel/harmony/build.sh
+++ kernel/harmony/build.sh
@@ -5,4 +5,6 @@ export CGO_ENABLED=1
 export GOOS=android
 export GOARCH=arm64
 export CC=/mnt/f/ohos-sdk-windows_linux-public/linux/native/llvm/bin/aarch64-unknown-linux-ohos-clang
+export CGO_FLAGS = "-I/mnt/f/log-adaptor/include"
+export CGO_LDFLAGS = "-L/mnt/f/log-adaptor//dist/arm64-v8a"
 go build --tags fts5 -ldflags "-s -w" -buildmode=c-shared -v -o libkernel.so .

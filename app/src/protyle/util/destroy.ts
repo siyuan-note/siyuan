e9bddf922477a47390e47f8b1a2342385c468332
Vanessa <lly219@gmail.com>
2024-12-13 11:14:42 +0800

    :art:

--- app/src/protyle/util/destroy.ts
+++ app/src/protyle/util/destroy.ts
@@ -1,14 +1,17 @@
 import {hideElements} from "../ui/hideElements";
+import {isSupportCSSHL} from "../render/searchMarkRender";
 
 export const destroy = (protyle: IProtyle) => {
     if (!protyle) {
         return;
     }
     hideElements(["util"], protyle);
-    protyle.highlight.markHL.clear();
-    protyle.highlight.mark.clear();
-    protyle.highlight.ranges = [];
-    protyle.highlight.rangeIndex = 0;
+    if (isSupportCSSHL()) {
+        protyle.highlight.markHL.clear();
+        protyle.highlight.mark.clear();
+        protyle.highlight.ranges = [];
+        protyle.highlight.rangeIndex = 0;
+    }
     protyle.observer?.disconnect();
     protyle.observerLoad?.disconnect();
     protyle.element.classList.remove("protyle");

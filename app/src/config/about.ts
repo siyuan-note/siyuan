5c60586b5824be9fcdd473deeb571811c4d4cf5d
Vanessa <lly219@gmail.com>
2024-12-18 16:53:28 +0800

    :art: https://github.com/siyuan-note/siyuan/issues/13522

--- app/src/config/about.ts
+++ app/src/config/about.ts
@@ -65,14 +65,6 @@ export const about = {
 </label>
 <label class="b3-label fn__flex">
     <div class="fn__flex-1">
-        ${window.siyuan.languages.about9}
-        <div class="b3-label__text">${window.siyuan.languages.about10}</div>
-    </div>
-    <div class="fn__space"></div>
-    <input class="b3-switch fn__flex-center" id="uploadErrLog" type="checkbox"${window.siyuan.config.system.uploadErrLog ? " checked" : ""}>
-</label>
-<label class="b3-label fn__flex">
-    <div class="fn__flex-1">
         ${window.siyuan.languages.about11}
         <div class="b3-label__text">${window.siyuan.languages.about12}</div>
     </div>
@@ -365,15 +357,6 @@ ${checkUpdateHTML}
                     cb() {
                         window.location.reload();
                     }
-                });
-            });
-        });
-        const uploadErrLogElement = about.element.querySelector("#uploadErrLog") as HTMLInputElement;
-        uploadErrLogElement.addEventListener("change", () => {
-            fetchPost("/api/system/setUploadErrLog", {uploadErrLog: uploadErrLogElement.checked}, () => {
-                exportLayout({
-                    errorExit: true,
-                    cb: exitSiYuan
                 });
             });
         });

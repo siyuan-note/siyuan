d928adab56838aa66e0772603b86c622530d4d1f
Vanessa <lly219@gmail.com>
2024-12-25 19:15:33 +0800

    :art: https://github.com/siyuan-note/siyuan/issues/13512

--- app/src/protyle/render/av/openMenuPanel.ts
+++ app/src/protyle/render/av/openMenuPanel.ts
@@ -1289,7 +1289,7 @@ export const openMenuPanel = (options: {
                     event.stopPropagation();
                     break;
                 } else if (type === "av-view-switch") {
-                    if (!target.querySelector(".b3-chip--primary")) {
+                    if (!target.parentElement.classList.contains("b3-menu__item--current")) {
                         options.blockElement.removeAttribute("data-render");
                         avRender(options.blockElement, options.protyle, undefined, target.parentElement.dataset.id);
                     }
@@ -1297,7 +1297,7 @@ export const openMenuPanel = (options: {
                     event.stopPropagation();
                     break;
                 } else if (type === "av-view-edit") {
-                    if (target.parentElement.querySelector(".b3-chip--primary")) {
+                    if (target.parentElement.classList.contains("b3-menu__item--current")) {
                         openViewMenu({
                             protyle: options.protyle,
                             blockElement: options.blockElement as HTMLElement,

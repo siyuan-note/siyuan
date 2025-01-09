ba2cd2828b72c236cebaa64252f387a7f4a609dc
Vanessa <lly219@gmail.com>
2025-01-09 11:45:48 +0800

    :art: https://github.com/siyuan-note/siyuan/issues/13763

--- app/src/protyle/util/paste.ts
+++ app/src/protyle/util/paste.ts
@@ -411,7 +411,7 @@ export const paste = async (protyle: IProtyle, event: (ClipboardEvent | DragEven
             e.removeAttribute(Constants.CUSTOM_RIFF_DECKS);
             e.classList.remove("protyle-wysiwyg--select", "protyle-wysiwyg--hl");
             e.setAttribute("updated", newId.split("-")[0]);
-            e.querySelector(".protyle-attr--refcount")?.remove();
+            e.removeAttribute("refcount");
             isBlock = true;
         });
         if (nodeElement.classList.contains("table")) {

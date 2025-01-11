b62384428b511840678de5b7b16f4310d34dfbbd
Daniel <845765@qq.com>
2025-01-11 16:48:26 +0800

    :art: Improve indexing

--- kernel/sql/database.go
+++ kernel/sql/database.go
@@ -741,6 +741,9 @@ func buildSpanFromNode(n *ast.Node, tree *parse.Tree, rootID, boxID, p string) (
 
 		if ast.NodeInlineHTML == n.Type {
 			// 没有行级 HTML，只有块级 HTML，这里转换为块
+			n.ID = ast.NewNodeID()
+			n.SetIALAttr("id", n.ID)
+			n.SetIALAttr("updated", n.ID[:14])
 			b, attrs := buildBlockFromNode(n, tree)
 			b.Type = ast.NodeHTMLBlock.String()
 			blocks = append(blocks, b)

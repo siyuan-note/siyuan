6075de1fbfd1fda8e7ade37fb2c550ac3244a607
Vanessa <lly219@gmail.com>
2025-02-03 11:56:14 +0800

    :art: https://github.com/siyuan-note/siyuan/pull/14000

--- app/src/protyle/hint/index.ts
+++ app/src/protyle/hint/index.ts
@@ -374,8 +374,8 @@ ${genHintItemHTML(item)}
             }
             lazyLoadEmojiImg(panelElement);
         } else {
-            // /emoji 菜单，min(402px,40vh) 和 .protyle-hint 保持一致，否则不显示底部导航栏
-            this.element.innerHTML = `<div style="padding:0;max-height:min(402px,40vh);width:368px" class="emojis">
+            // max-height：min(402px,40vh) 和 .protyle-hint 保持一致，否则 emoji 不显示底部导航
+            this.element.innerHTML = `<div style="padding:0;max-height:min(402px,40vh);width:366px" class="emojis">
 <div class="emojis__panel">${filterEmoji(value, 256)}</div>
 <div class="fn__flex${value ? " fn__none" : ""}">
     ${[

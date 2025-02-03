7f9d00fbf1a2f3f56f6a4545f08577a6e5dc3e43
Jeffrey Chen <78434827+TCOTC@users.noreply.github.com>
2025-02-03 17:27:06 +0800

    fix: 在行级标签上选择文本时不打开搜索 (#13976)
    fix https://github.com/siyuan-note/siyuan/issues/13862

--- app/src/protyle/wysiwyg/index.ts
+++ app/src/protyle/wysiwyg/index.ts
@@ -2388,7 +2388,7 @@ export class WYSIWYG {
             }
 
             const tagElement = hasClosestByAttribute(event.target, "data-type", "tag");
-            if (tagElement && !event.altKey && !event.shiftKey) {
+            if (tagElement && !event.altKey && !event.shiftKey && range.toString() === "") {
                 /// #if !MOBILE
                 openGlobalSearch(protyle.app, `#${tagElement.textContent}#`, !ctrlIsPressed, {method: 0});
                 hideElements(["dialog"]);

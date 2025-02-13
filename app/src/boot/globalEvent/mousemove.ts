2e6fd63c71720766c7f3cacc1326d01fa51ed408
Vanessa <lly219@gmail.com>
2025-02-13 11:50:30 +0800

    :art: https://github.com/siyuan-note/siyuan/issues/13938

--- app/src/boot/globalEvent/mousemove.ts
+++ app/src/boot/globalEvent/mousemove.ts
@@ -58,6 +58,7 @@ export const windowMouseMove = (event: MouseEvent & { target: HTMLElement }, mou
                 if (event.clientY > document.getElementById("toolbar").clientHeight &&
                     event.clientY < window.innerHeight - document.getElementById("status").clientHeight - document.getElementById("dockBottom").clientHeight) {
                     if (!hasClosestByClassName(event.target, "b3-menu") && !hasClosestByClassName(event.target, "protyle-toolbar") &&
+                        !hasClosestByClassName(event.target, "b3-dialog") &&
                         !hasClosestByClassName(event.target, "layout--float")) {
                         window.siyuan.layout.leftDock.showDock();
                     }
@@ -70,7 +71,8 @@ export const windowMouseMove = (event: MouseEvent & { target: HTMLElement }, mou
                 (window.siyuan.layout.rightDock.element.clientWidth > 0 || (window.siyuan.layout.rightDock.element.clientWidth === 0 && event.clientX > window.innerWidth - 8))) {
                 if (event.clientY > document.getElementById("toolbar").clientHeight &&
                     event.clientY < window.innerHeight - document.getElementById("status").clientHeight - document.getElementById("dockBottom").clientHeight) {
-                    if (!hasClosestByClassName(event.target, "layout--float") && !hasClosestByClassName(event.target, "protyle-toolbar")) {
+                    if (!hasClosestByClassName(event.target, "layout--float") && !hasClosestByClassName(event.target, "protyle-toolbar") &&
+                        !hasClosestByClassName(event.target, "b3-dialog")) {
                         window.siyuan.layout.rightDock.showDock();
                     }
                 } else {

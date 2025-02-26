0b4cff93acd3b4f9716c814de1169a346685b731
Jeffrey Chen <78434827+TCOTC@users.noreply.github.com>
2025-02-26 09:54:59 +0800

    Improve tooltip (#13966)
    * Improve tooltip
    
    * fix: https://github.com/siyuan-note/siyuan/issues/13971
    
    * 重构逻辑，非必要不操作元素
    
    * https://github.com/siyuan-note/siyuan/issues/13971
    
    * fix https://ld246.com/article/1740361496784
    
    * fix https://github.com/siyuan-note/siyuan/issues/13962

--- app/src/card/makeCard.ts
+++ app/src/card/makeCard.ts
@@ -25,7 +25,7 @@ export const genCardItem = (item: ICardPackage) => {
 <span data-type="view" class="b3-list-item__action b3-tooltips b3-tooltips__w" aria-label="${window.siyuan.languages.cardPreview}">
     <svg><use xlink:href="#iconEye"></use></svg>
 </span>
-<span data-type="remove" class="b3-list-item__action b3-tooltips b3-tooltips__w" aria-label="${window.siyuan.languages.removeDeck}">
+<span data-type="remove" class="b3-list-item__action b3-list-item__action--warning b3-tooltips b3-tooltips__w" aria-label="${window.siyuan.languages.removeDeck}">
     <svg><use xlink:href="#iconMin"></use></svg>
 </span>
 <span data-type="add" style="display: flex" class="b3-list-item__action b3-tooltips b3-tooltips__w" aria-label="${window.siyuan.languages.addDeck}">

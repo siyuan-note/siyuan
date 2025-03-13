73efbe9737e9d2f15f351d84da94fe94668ad797
Vanessa <lly219@gmail.com>
2025-03-13 17:15:32 +0800

    :art: https://github.com/siyuan-note/siyuan/issues/14326

--- app/src/protyle/render/blockRender.ts
+++ app/src/protyle/render/blockRender.ts
@@ -23,6 +23,7 @@ export const blockRender = (protyle: IProtyle, element: Element, top?: number) =
         }
         // 需置于请求返回前，否则快速滚动会导致重复加载 https://ld246.com/article/1666857862494?r=88250
         item.setAttribute("data-render", "true");
+        item.querySelector(".protyle-wysiwyg__embed")?.remove();
         genRenderFrame(item);
         item.style.height = (item.clientHeight - 4) + "px"; // 减少抖动 https://ld246.com/article/1668669380171
         const content = Lute.UnEscapeHTMLStr(item.getAttribute("data-content"));

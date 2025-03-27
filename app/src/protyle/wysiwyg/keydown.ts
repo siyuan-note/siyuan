708d32b6685eb742891b81322dba9ccbe6576b5b
Vanessa <lly219@gmail.com>
2025-03-27 23:06:20 +0800

    :art: https://github.com/siyuan-note/siyuan/issues/14443

--- app/src/protyle/wysiwyg/keydown.ts
+++ app/src/protyle/wysiwyg/keydown.ts
@@ -704,11 +704,9 @@ export const keydown = (protyle: IProtyle, editorElement: HTMLElement) => {
                                 } else {
                                     // 修正光标上移至 \n 结尾的块时落点错误 https://github.com/siyuan-note/siyuan/issues/14443
                                     const prevEditableElement = getContenteditableElement(previousElement) as HTMLElement;
-                                    if (prevEditableElement && prevEditableElement.innerText.endsWith("\n")) {
-                                        focusBlock(previousElement, undefined, false);
-                                        scrollCenter(protyle, previousElement);
-                                        event.stopPropagation();
-                                        event.preventDefault();
+                                    if (prevEditableElement &&  prevEditableElement.lastChild.nodeType === 3 &&
+                                        prevEditableElement.lastChild.textContent.endsWith("\n")) {
+                                        prevEditableElement.lastChild.textContent = prevEditableElement.lastChild.textContent.replace(/\n$/, "");
                                     }
                                 }
                             }

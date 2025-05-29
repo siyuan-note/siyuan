a26823a84c22bf06e19986f1b1141b88f2890e0c
Daniel <845765@qq.com>
2025-05-29 17:12:12 +0800

    :art: Export path selection problem on Linux desktop https://github.com/siyuan-note/siyuan/issues/14861

--- app/electron/main.js
+++ app/electron/main.js
@@ -652,6 +652,7 @@ app.commandLine.appendSwitch("disable-web-security");
 app.commandLine.appendSwitch("auto-detect", "false");
 app.commandLine.appendSwitch("no-proxy-server");
 app.commandLine.appendSwitch("enable-features", "PlatformHEVCDecoderSupport");
+app.commandLine.appendSwitch("xdg-portal-required-version", "4");
 
 // Support set Chromium command line arguments on the desktop https://github.com/siyuan-note/siyuan/issues/9696
 writeLog("app is packaged [" + app.isPackaged + "], command line args [" + process.argv.join(", ") + "]");

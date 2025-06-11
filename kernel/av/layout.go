1bba2ab20580be3adeabba47407c563a65c5eec4
Daniel <845765@qq.com>
2025-06-11 17:40:02 +0800

    :art: Database gallery view https://github.com/siyuan-note/siyuan/issues/10414

--- kernel/av/layout.go
+++ kernel/av/layout.go
@@ -16,17 +16,6 @@
 
 package av
 
-// Item 描述了一个项目的接口。
-// 项目可以表格行、画廊卡片或其他视图类型的实体。
-type Item interface {
-
-	// GetBlockValue 返回主键的值。
-	GetBlockValue() *Value
-
-	// GetID 返回项目的 ID。
-	GetID() string
-}
-
 // BaseLayout 描述了布局的基础结构。
 type BaseLayout struct {
 	Spec     int           `json:"spec"`     // 布局格式版本
@@ -72,4 +61,15 @@ type BaseInstanceField struct {
 	Relation     *Relation       `json:"relation,omitempty"` // 关联字段
 	Rollup       *Rollup         `json:"rollup,omitempty"`   // 汇总字段
 	Date         *Date           `json:"date,omitempty"`     // 日期设置
+}
+
+// Item 描述了一个项目的接口。
+// 项目可以表格行、画廊卡片或其他视图类型的实体。
+type Item interface {
+
+	// GetBlockValue 返回主键的值。
+	GetBlockValue() *Value
+
+	// GetID 返回项目的 ID。
+	GetID() string
 }

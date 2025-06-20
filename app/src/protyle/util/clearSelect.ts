8bbee09614cfa9e1a230fcd5a87aba395ec755d8
Vanessa <lly219@gmail.com>
2025-06-20 11:03:47 +0800

    :art: https://github.com/siyuan-note/siyuan/issues/10414

---
+++ app/src/protyle/util/clearSelect.ts
@@ -0,0 +1,35 @@
+import {updateHeader} from "../render/av/row";
+
+export const clearSelect = (types: ("av" | "img" | "cell" | "row")[], element: Element) => {
+    if (types.includes("cell")) {
+        element.querySelectorAll(".av__cell--select, .av__cell--active").forEach((item: HTMLElement) => {
+            item.querySelector(".av__drag-fill")?.remove();
+            item.classList.remove("av__cell--select", "av__cell--active");
+        });
+    }
+    if (types.includes("row")) {
+        element.querySelectorAll(".av__row--select").forEach((item: HTMLElement) => {
+            item.classList.remove("av__row--select");
+            item.querySelector(".av__firstcol use").setAttribute("xlink:href", "#iconUncheck");
+            updateHeader(item);
+        });
+    }
+    if (types.includes("av")) {
+        element.querySelectorAll(" .av__cell--select, .av__cell--active, .av__row--select, .av__gallery-item--select").forEach((item: HTMLElement) => {
+            if (item.classList.contains("av__row--select")) {
+                item.classList.remove("av__row--select");
+                item.querySelector(".av__firstcol use").setAttribute("xlink:href", "#iconUncheck");
+                updateHeader(item);
+            } else {
+                item.querySelector(".av__drag-fill")?.remove();
+                item.classList.remove("av__cell--select", "av__cell--active", "av__gallery-item--select");
+            }
+        });
+    }
+    if (types.includes("img")) {
+        element.querySelectorAll(".img--select").forEach((item: HTMLElement) => {
+            item.classList.remove("img--select");
+        });
+    }
+
+};

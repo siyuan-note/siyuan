04f81c4deeceaaf0651b9e6134f4bbd0d778219e
Daniel <845765@qq.com>
2025-06-26 18:41:09 +0800

    :art: Replace \\u0000 with "" when writing tree data

--- kernel/filesys/tree.go
+++ kernel/filesys/tree.go
@@ -242,6 +242,7 @@ func prepareWriteTree(tree *parse.Tree) (data []byte, filePath string, err error
 	tree.Root.SetIALAttr("type", "doc")
 	renderer := render.NewJSONRenderer(tree, luteEngine.RenderOptions)
 	data = renderer.Render()
+	data = bytes.ReplaceAll(data, []byte("\\u0000"), []byte(""))
 
 	if !util.UseSingleLineSave {
 		buf := bytes.Buffer{}

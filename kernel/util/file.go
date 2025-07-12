3ffc32d089392455363d3d253b00eb426f9bacf5
Daniel <845765@qq.com>
2025-07-12 10:35:55 +0800

    :art: Improve mime type detect

--- kernel/util/file.go
+++ kernel/util/file.go
@@ -78,7 +78,13 @@ func GetUniqueFilename(filePath string) string {
 func GetMimeTypeByExt(filePath string) (ret string) {
 	ret = mime.TypeByExtension(filepath.Ext(filePath))
 	if "" == ret {
-		m, err := mimetype.DetectFile(filePath)
+		f, err := filelock.OpenFile(filePath, os.O_RDONLY, 0644)
+		if err != nil {
+			logging.LogErrorf("open file [%s] failed: %s", filePath, err)
+			return
+		}
+		defer filelock.CloseFile(f)
+		m, err := mimetype.DetectReader(f)
 		if err != nil {
 			logging.LogErrorf("detect mime type of [%s] failed: %s", filePath, err)
 			return

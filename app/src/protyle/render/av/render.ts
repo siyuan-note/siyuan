eb0b76bc30fa8e0931fac629c603859a6e601410
Vanessa <lly219@gmail.com>
2025-07-15 17:24:58 +0800

    :art: https://github.com/siyuan-note/siyuan/issues/15279

--- app/src/protyle/render/av/render.ts
+++ app/src/protyle/render/av/render.ts
@@ -93,7 +93,8 @@ export const avRender = (element: Element, protyle: IProtyle, cb?: (data: IAV) =
                 snapshot,
                 pageSize: parseInt(e.dataset.pageSize) || undefined,
                 viewID: e.getAttribute(Constants.CUSTOM_SY_AV_VIEW) || "",
-                query: query.trim()
+                query: query.trim(),
+                blockID: e.getAttribute("data-node-id"),
             }, (response) => {
                 const data = response.data.view as IAVTable;
                 if (response.data.viewType === "gallery") {

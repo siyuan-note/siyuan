d6c1761cbc4ec28a31b799e28cd3ef3e4bd3508f
Tron <achuan-2@outlook.com>
2025-07-22 14:55:29 +0800

    支持渲染queryBlocks的sql语句和嵌入块sql语句

--- app/stage/protyle/js/highlight.js/third-languages.js
+++ app/stage/protyle/js/highlight.js/third-languages.js
@@ -138,16 +138,14 @@ hljs.registerLanguage("gdscript",function(){"use strict";var e=e||{};function r(
 
 // https://github.com/siyuan-note/siyuan/pull/15345
 hljs.registerLanguage('template', function (hljs) {
-    const markdown = hljs.getLanguage('markdown');
-    const go = hljs.getLanguage('go');
 
-    const markdownRules = markdown || { contains: [] };
-    const goRules = go || { contains: [] };
+    const markdownRules = hljs.getLanguage('markdown') || { contains: [] };
+    const goRules = hljs.getLanguage('go') || { contains: [] };
 
     // 内置函数规则
     const BUILT_IN_FUNCTIONS = {
         className: 'built_in',
-        begin: /\b(queryBlocks|querySpans|parseTime|Weekday|WeekdayCN|WeekdayCN2|ISOWeek|pow|powf|log|logf|FormatFloat|now|date|toDate|duration|AddDate|Sub|sub|add|mul|mod|div|min|max|Compare|Year|Month|Day|Hour|Minute|Second|Hours|Minutes|Seconds|String|trim|repeat|substr|trunc|abbrev|contains|cat|replace|join|splitList|list|first|last|append|prepend|concat|reverse|has|index|slice|len|atoi|float64|int|int64|toDecimal|toString|toStrings|dict|get)\b/,
+        begin: /\b(queryBlocks|querySpans|querySQL|parseTime|Weekday|WeekdayCN|WeekdayCN2|ISOWeek|pow|powf|log|logf|FormatFloat|now|date|toDate|duration|AddDate|Sub|sub|add|mul|mod|div|min|max|Compare|Year|Month|Day|Hour|Minute|Second|Hours|Minutes|Seconds|String|trim|repeat|substr|trunc|abbrev|contains|cat|replace|join|splitList|list|first|last|append|prepend|concat|reverse|has|index|slice|len|atoi|float64|int|int64|toDecimal|toString|toStrings|dict|get)\b/,
         relevance: 10
     };
 
@@ -165,42 +163,168 @@ hljs.registerLanguage('template', function (hljs) {
         relevance: 10
     };
 
-
-
-    // 模板内容的公共规则
-    const TEMPLATE_CONTENT = [
-        VARIABLE_RULE,
-        BUILT_IN_FUNCTIONS,
-        KEYWORDS_OPERATORS,
-        ...goRules.contains,
+    // SQL 内容的通用规则
+    const SQL_CONTENT_RULES = [
+        // SQL 关键字 - 大小写不敏感
+        {
+            className: 'keyword',
+            begin: /\b(SELECT|FROM|WHERE|AND|OR|ORDER|BY|GROUP|HAVING|INSERT|UPDATE|DELETE|CREATE|DROP|ALTER|INDEX|TABLE|DATABASE|LIKE|IN|NOT|IS|NULL|DISTINCT|LIMIT|OFFSET|JOIN|INNER|LEFT|RIGHT|OUTER|ON|AS|COUNT|SUM|AVG|MIN|MAX|CASE|WHEN|THEN|ELSE|END)\b/i,
+            relevance: 10
+        },
+        // SQL 函数 - 大小写不敏感
+        {
+            className: 'built_in',
+            begin: /\b(COUNT|SUM|AVG|MIN|MAX|LENGTH|UPPER|LOWER|SUBSTR|TRIM|REPLACE|CONCAT)\b/i,
+            relevance: 8
+        },
+        // SQL 中的字符串 (单引号)
+        {
+            className: 'string',
+            begin: /'/,
+            end: /'/,
+            contains: [{ begin: /''/ }]
+        },
+        // 数字
+        {
+            className: 'number',
+            begin: /\b\d+(\.\d+)?\b/,
+            relevance: 0
+        },
+        // 思源笔记特定的字段
+        {
+            className: 'attr',
+            begin: /\b(id|parent_id|root_id|hash|box|path|hpath|name|alias|memo|tag|content|fcontent|markdown|length|type|subtype|ial|sort|created|updated)\b/,
+            relevance: 8
+        },
+        // 问号占位符
+        {
+            className: 'symbol',
+            begin: /\?/,
+            relevance: 5
+        },
+        // .action{} 嵌套在 SQL 中
+        {
+            begin: /(\.action\{)/,
+            end: /(\})/,
+            beginScope: 'selector-pseudo',
+            endScope: 'selector-pseudo',
+            contains: [VARIABLE_RULE, BUILT_IN_FUNCTIONS, KEYWORDS_OPERATORS],
+            relevance: 10
+        },
     ];
 
-    // .action 块规则 - 只高亮边界
+    // 直接的 SQL 语句规则 - 用于双大括号中的完整SQL
+    const DIRECT_SQL_STATEMENT = {
+        // 匹配以 SELECT 开头的语句
+        begin: /\b(SELECT)\b/i,
+        end: /(?=\}\}|$)/,
+        returnBegin: true,
+        className: 'string',
+        contains: SQL_CONTENT_RULES,
+        relevance: 20
+    };
+
+    // SQL 字符串规则 - 专门用于包含 SQL 语句的字符串
+    const SQL_STRING = {
+        className: 'string',
+        begin: /"/,
+        end: /"/,
+        contains: SQL_CONTENT_RULES,
+        relevance: 8
+    };
+
+    // 检测 SQL 查询函数后的字符串
+    const SQL_FUNCTION_CALL = {
+        begin: /\b(queryBlocks|querySpans|querySQL)\s+/,
+        end: /(?=\s+\$|\s*\)|\s*$)/,
+        returnBegin: true,
+        contains: [
+            // 函数名
+            {
+                className: 'built_in',
+                begin: /\b(queryBlocks|querySpans|querySQL)\b/,
+                relevance: 10
+            },
+            // 跟在函数名后的 SQL 字符串
+            SQL_STRING
+        ],
+        relevance: 15
+    };
+
+    // 普通字符串规则
+    const STRING_RULE = {
+        className: 'string',
+        variants: [
+            { begin: /"/, end: /"/ },
+            { begin: /'/, end: /'/ }
+        ],
+        relevance: 5
+    };
+
+    // 注释规则
+    const COMMENT_RULE = {
+        className: 'comment',
+        variants: [
+            { begin: /\/\*/, end: /\*\// },
+            { begin: /\/\//, end: /$/ }
+        ],
+        relevance: 10
+    };
+
+    // .action 块规则
     const ACTION_BLOCK = {
         begin: /(\.action\{)/,
         end: /(\})/,
         beginScope: 'selector-pseudo',
         endScope: 'selector-pseudo',
-        contains: TEMPLATE_CONTENT,
+        contains: [
+            COMMENT_RULE,
+            SQL_FUNCTION_CALL, // SQL 函数调用（优先级较高）
+            VARIABLE_RULE,
+            BUILT_IN_FUNCTIONS,
+            KEYWORDS_OPERATORS,
+            STRING_RULE, // 普通字符串（优先级较低）
+            // 数字
+            {
+                className: 'number',
+                begin: /\b\d+(\.\d+)?\b/,
+                relevance: 0
+            },
+            // 操作符
+            {
+                className: 'operator',
+                begin: /(:=|==|!=|<=|>=|<|>|=)/,
+                relevance: 5
+            },
+            ...goRules.contains,
+        ],
         relevance: 10
     };
 
-    // 双大括号块规则 - 只高亮边界
+    // 双大括号块规则 - 增强 SQL 支持
     const CURLY_BLOCK = {
         begin: /(\{\{)/,
         end: /(\}\})/,
         beginScope: 'selector-pseudo',
         endScope: 'selector-pseudo',
-        contains: TEMPLATE_CONTENT,
+        contains: [
+            COMMENT_RULE,
+            DIRECT_SQL_STATEMENT, // 直接的SQL语句（最高优先级）
+            VARIABLE_RULE,
+            BUILT_IN_FUNCTIONS,
+            KEYWORDS_OPERATORS,
+            STRING_RULE,
+            ...goRules.contains,
+        ],
         relevance: 10
     };
-    // 思源块属性设置语法 - 贪婪匹配
+
+    // 思源块属性规则
     const BLOCK_ATTR_RULE = {
         className: 'comment',
         begin: /\{:/,
-        end: /\}(?=\s*$)/,  // 匹配行尾附近的}
+        end: /\}(?=\s*$)/,
         contains: [
-
             {
                 className: 'string',
                 begin: /\"/,
@@ -211,25 +335,28 @@ hljs.registerLanguage('template', function (hljs) {
                         end: /(\})/,
                         beginScope: 'selector-pseudo',
                         endScope: 'selector-pseudo',
-                        contains: TEMPLATE_CONTENT,
+                        contains: [
+                            VARIABLE_RULE,
+                            BUILT_IN_FUNCTIONS,
+                            KEYWORDS_OPERATORS,
+                            STRING_RULE,
+                        ],
                         relevance: 10
                     },
                 ]
             },
             {
                 className: 'title',
-                begin: /\b\w+=/,  // 属性名=值的格式
+                begin: /\b\w+=/,
             }
         ],
         relevance: 10
     };
 
-
-
     return {
         name: 'template',
         aliases: ['siyuan-template', 'template'],
-        case_insensitive: false,
+        case_insensitive: true,
         contains: [
             ACTION_BLOCK,
             CURLY_BLOCK,
@@ -237,4 +364,5 @@ hljs.registerLanguage('template', function (hljs) {
             ...markdownRules.contains,
         ]
     };
+
 });

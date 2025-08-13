b78e2edfb845e5117fbec38f29f269af2e44e462
Daniel <845765@qq.com>
2025-08-13 23:35:16 +0800

    :art: Improve av

--- kernel/model/export.go
+++ kernel/model/export.go
@@ -3442,7 +3442,7 @@ func getAttrViewTable(attrView *av.AttributeView, view *av.View, query string) (
 	}
 
 	depth := 1
-	ret = sql.RenderAttributeViewTable(attrView, view, query, &depth)
+	ret = sql.RenderAttributeViewTable(attrView, view, query, &depth, map[string]*av.AttributeView{})
 	return
 }
 

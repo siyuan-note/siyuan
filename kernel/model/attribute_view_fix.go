202ccfa5cfb7c3c842ce08c5d6533a5cab56aebc
Daniel <845765@qq.com>
2025-08-24 15:16:10 +0800

    :art: Limit the database title to 512 characters https://github.com/siyuan-note/siyuan/issues/15459

--- kernel/model/attribute_view_fix.go
+++ kernel/model/attribute_view_fix.go
@@ -101,10 +101,7 @@ func checkAttrView(attrView *av.AttributeView, view *av.View) {
 		}
 	}
 
-	if strings.Contains(attrView.Name, "\n") {
-		attrView.Name = strings.ReplaceAll(attrView.Name, "\n", " ")
-	}
-
+	attrView.Name = strings.ReplaceAll(attrView.Name, "\n", " ")
 	// 截断超长的数据库标题 Limit the database title to 512 characters https://github.com/siyuan-note/siyuan/issues/15459
 	if 512 < utf8.RuneCountInString(attrView.Name) {
 		attrView.Name = gulu.Str.SubStr(attrView.Name, 512)

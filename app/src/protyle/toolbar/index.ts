4aede9687ef0e33fa367634cbadc1c20fce306c6
Jeffrey Chen <78434827+TCOTC@users.noreply.github.com>
2025-10-12 18:31:10 +0800

    Improve toolbar position calculation (#15983)
    * :art: Improve toolbar position calculation
    
    * :art: Improve toolbar position calculation
    
    * :art: Improve toolbar position calculation

--- app/src/protyle/toolbar/index.ts
+++ app/src/protyle/toolbar/index.ts
@@ -164,12 +164,17 @@ export class Toolbar {
             this.element.classList.add("fn__none");
             return;
         }
-        const rangePosition = getSelectionPosition(nodeElement, range);
-        this.element.classList.remove("fn__none");
+        this.element.classList.remove("fn__none", "protyle-toolbar__transition");
+        
         this.toolbarHeight = this.element.clientHeight;
-        const y = rangePosition.top - this.toolbarHeight - 4;
+        const rangePosition = getSelectionPosition(nodeElement, range, this.element.clientWidth);
+        const y = rangePosition.isToolbarAtBottom ?
+            Math.min(rangePosition.top + 4, protyle.element.getBoundingClientRect().bottom - this.toolbarHeight) :
+            Math.max(rangePosition.top - this.toolbarHeight - 4, protyle.element.getBoundingClientRect().top + 30);
         this.element.setAttribute("data-inity", y + Constants.ZWSP + protyle.contentElement.scrollTop.toString());
-        setPosition(this.element, rangePosition.left - 52, Math.max(y, protyle.element.getBoundingClientRect().top + 30));
+        setPosition(this.element, rangePosition.left, y);
+
+        this.element.classList.add("protyle-toolbar__transition");
         this.element.querySelectorAll(".protyle-toolbar__item--current").forEach(item => {
             item.classList.remove("protyle-toolbar__item--current");
         });

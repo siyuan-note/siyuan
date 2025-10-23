9366b0f0738bbb6cd2c0c4a2f962cb4fa3196eb1
Vanessa <lly219@gmail.com>
2025-10-23 12:41:06 +0800

    :art: https://github.com/siyuan-note/siyuan/pull/15011

--- app/src/layout/util.ts
+++ app/src/layout/util.ts
@@ -987,19 +987,22 @@ export const adjustLayout = (layout: Layout = window.siyuan.layout.centerLayout.
         }
     });
     let lastItem: HTMLElement;
-    let index = Math.floor(window.innerWidth / 24);
-    // +2 由于某些分辨率下 scrollWidth 会大于 clientWidth
-    while (layout.element.scrollWidth > layout.element.clientWidth + 2 && index > 0) {
-        layout.children.find((item: Layout | Wnd) => {
-            if (item.element.style.width && item.element.style.width !== "0px") {
-                item.element.style.maxWidth = Math.max(Math.min(item.element.clientWidth, window.innerWidth) - 8, 64) + "px";
-                lastItem = item.element;
+    let index = Math.ceil(screen.width / 8);
+    if (layout.direction === "lr") {
+        while (index > 0) {
+            let width = 0;
+            layout.children.find((item: Layout | Wnd) => {
+                if (item.element.style.width && item.element.style.width !== "0px") {
+                    item.element.style.maxWidth = Math.max(Math.min(item.element.clientWidth, window.innerWidth) - 8, 64) + "px";
+                    lastItem = item.element;
+                }
+                width += item.element.clientWidth;
+            });
+            index--;
+            if (width <= layout.element.clientWidth) {
+                break;
             }
-            if (layout.element.scrollWidth <= layout.element.clientWidth + 2) {
-                return true;
-            }
-        });
-        index--;
+        }
     }
     if (lastItem) {
         lastItem.style.maxWidth = Math.max(Math.min(lastItem.clientWidth, window.innerWidth) - 8, 64) + "px";

72acce2c8655652e20737b3c69c8684ea76f5b3e
Vanessa <lly219@gmail.com>
2025-11-11 10:45:31 +0800

    :art: https://github.com/siyuan-note/siyuan/issues/16240

--- app/src/card/openCard.ts
+++ app/src/card/openCard.ts
@@ -388,7 +388,7 @@ export const bindCardEvent = async (options: {
                                 } else {
                                     options.cardsData.unreviewedOldCardCount--;
                                 }
-                                options.element.dispatchEvent(new CustomEvent("click", {detail: "0"}));
+                                options.element.firstElementChild.dispatchEvent(new CustomEvent("click", {detail: "0"}));
                                 options.cardsData.cards.splice(index, 1);
                                 index--;
                                 timedialog.destroy();
@@ -444,7 +444,7 @@ export const bindCardEvent = async (options: {
                         } else {
                             options.cardsData.unreviewedOldCardCount--;
                         }
-                        options.element.dispatchEvent(new CustomEvent("click", {detail: "0"}));
+                        options.element.firstElementChild.dispatchEvent(new CustomEvent("click", {detail: "0"}));
                         transaction(undefined, [{
                             action: "removeFlashcards",
                             deckID: Constants.QUICK_DECK_ID,

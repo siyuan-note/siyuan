71a56db7aa315f22259603ab3c7c851355a0b200
Vanessa <lly219@gmail.com>
2025-11-13 19:04:18 +0800

    :art:

--- app/src/config/appearance.ts
+++ app/src/config/appearance.ts
@@ -250,37 +250,38 @@ export const appearance = {
                 title: "\uD83D\uDD07 " + window.siyuan.languages.appearance18,
                 content: `<div class="fn__hr"></div>
 <div class="b3-label">
-${window.siyuan.languages.statusBarMsgPushTip}
-</div>
-<div class="b3-tab-bar b3-list b3-list--background">
-    <label class="b3-list-item">
-        <div class="b3-list-item__text">
-           ${window.siyuan.languages["_taskAction"]["task.database.index.commit"]}
-        </div>
-        <span class="fn__space"></span>
-        <input class="b3-switch fn__flex-center" id="msgTaskDatabaseIndexCommitDisabled" type="checkbox"${window.siyuan.config.appearance.statusBar.msgTaskDatabaseIndexCommitDisabled ? "" : " checked"}>
-    </label>    
-    <label class="b3-list-item">
-        <div class="b3-list-item__text">
-           ${window.siyuan.languages["_taskAction"]["task.asset.database.index.commit"]}
-        </div>
-        <span class="fn__space"></span>
-        <input class="b3-switch fn__flex-center" id="msgTaskAssetDatabaseIndexCommitDisabled" type="checkbox"${window.siyuan.config.appearance.statusBar.msgTaskAssetDatabaseIndexCommitDisabled ? "" : " checked"}>
-    </label>
-    <label class="b3-list-item">
-        <div class="b3-list-item__text">
-           ${window.siyuan.languages["_taskAction"]["task.history.database.index.commit"]}
-        </div>
-        <span class="fn__space"></span>
-        <input class="b3-switch fn__flex-center" id="msgTaskHistoryDatabaseIndexCommitDisabled" type="checkbox"${window.siyuan.config.appearance.statusBar.msgTaskHistoryDatabaseIndexCommitDisabled ? "" : " checked"}>
-    </label>
-    <label class="b3-list-item">
-        <div class="b3-list-item__text">
-           ${window.siyuan.languages["_taskAction"]["task.history.generateFile"]}
-        </div>
-        <span class="fn__space"></span>
-        <input class="b3-switch fn__flex-center" id="msgTaskHistoryGenerateFileDisabled" type="checkbox"${window.siyuan.config.appearance.statusBar.msgTaskHistoryGenerateFileDisabled ? "" : " checked"}>
-    </label>
+    ${window.siyuan.languages.statusBarMsgPushTip}
+    <div class="fn__hr"></div>
+    <div class="b3-tab-bar b3-list b3-list--background">
+        <label class="b3-list-item">
+            <div class="fn__flex-1 ft__on-surface">
+               ${window.siyuan.languages["_taskAction"]["task.database.index.commit"]}
+            </div>
+            <span class="fn__space"></span>
+            <input class="b3-switch fn__flex-center" id="msgTaskDatabaseIndexCommitDisabled" type="checkbox"${window.siyuan.config.appearance.statusBar.msgTaskDatabaseIndexCommitDisabled ? "" : " checked"}>
+        </label>    
+        <label class="b3-list-item">
+            <div class="fn__flex-1 ft__on-surface">
+               ${window.siyuan.languages["_taskAction"]["task.asset.database.index.commit"]}
+            </div>
+            <span class="fn__space"></span>
+            <input class="b3-switch fn__flex-center" id="msgTaskAssetDatabaseIndexCommitDisabled" type="checkbox"${window.siyuan.config.appearance.statusBar.msgTaskAssetDatabaseIndexCommitDisabled ? "" : " checked"}>
+        </label>
+        <label class="b3-list-item">
+            <div class="fn__flex-1 ft__on-surface">
+               ${window.siyuan.languages["_taskAction"]["task.history.database.index.commit"]}
+            </div>
+            <span class="fn__space"></span>
+            <input class="b3-switch fn__flex-center" id="msgTaskHistoryDatabaseIndexCommitDisabled" type="checkbox"${window.siyuan.config.appearance.statusBar.msgTaskHistoryDatabaseIndexCommitDisabled ? "" : " checked"}>
+        </label>
+        <label class="b3-list-item">
+            <div class="fn__flex-1 ft__on-surface">
+               ${window.siyuan.languages["_taskAction"]["task.history.generateFile"]}
+            </div>
+            <span class="fn__space"></span>
+            <input class="b3-switch fn__flex-center" id="msgTaskHistoryGenerateFileDisabled" type="checkbox"${window.siyuan.config.appearance.statusBar.msgTaskHistoryGenerateFileDisabled ? "" : " checked"}>
+        </label>
+    </div>
 </div>`
             });
 

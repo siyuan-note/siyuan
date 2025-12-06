e8e36185da51976777e835b9a510f57c3f6799c2
Daniel <845765@qq.com>
2025-12-06 16:55:27 +0800

    :sparkles: Callout block https://github.com/siyuan-note/siyuan/issues/16051
    Signed-off-by: Daniel <845765@qq.com>

--- kernel/model/storage.go
+++ kernel/model/storage.go
@@ -338,7 +338,7 @@ type CriterionTypes struct {
 	VideoBlock    bool `json:"videoBlock"`
 	IFrameBlock   bool `json:"iframeBlock"`
 	WidgetBlock   bool `json:"widgetBlock"`
-	Call          bool `json:"callout"`
+	Callout       bool `json:"callout"`
 }
 
 type CriterionReplaceTypes struct {

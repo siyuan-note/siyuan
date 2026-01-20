e6fa4dd0e138c63f783ff29f9932e46f5178eb36
Vanessa <lly219@gmail.com>
2026-01-20 09:25:40 +0800

    :recycle: https://github.com/siyuan-note/siyuan/issues/14972

--- app/src/protyle/util/paste.ts
+++ app/src/protyle/util/paste.ts
@@ -361,7 +361,7 @@ export const paste = async (protyle: IProtyle, event: (ClipboardEvent | DragEven
         }
         return;
     }
-    protyle.hint.enableExtend = Constants.BLOCK_HINT_KEYS.concat("{{", "/", "#", "、", "「「", "「『", "『「", "『『", "{{").includes(protyle.hint.splitChar);
+    protyle.hint.enableExtend = Constants.BLOCK_HINT_KEYS.concat("{{", "/", "#", "、", "「「", "「『", "『「", "『『",).includes(protyle.hint.splitChar);
     hideElements(protyle.hint.enableExtend ? ["select"] : ["select", "hint"], protyle);
     protyle.wysiwyg.element.querySelectorAll(".protyle-wysiwyg--hl").forEach(item => {
         item.classList.remove("protyle-wysiwyg--hl");

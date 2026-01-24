f74bb133280ff3333705527a8d573d81ccbd7efc
Davide Garberi <dade.garberi@gmail.com>
2026-01-24 22:03:26 +0100

    Add use TLS for network serving configuration option

--- app/src/types/config.d.ts
+++ app/src/types/config.d.ts
@@ -1624,6 +1624,10 @@ declare namespace Config {
          */
         networkServe: boolean;
         /**
+         * Whether to enable HTTPS for network serve (TLS encryption)
+         */
+        networkServeTLS: boolean;
+        /**
          * The operating system name determined at compile time (obtained using the command `go tool
          * dist list`)
          * - `android`: Android

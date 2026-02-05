0b439c722803ae62c6a8dbf1388e862cf3fa2a30
Vanessa <lly219@gmail.com>
2026-02-05 21:57:45 +0800

    :art: 移动端输入法

--- app/src/mobile/util/keyboardToolbar.ts
+++ app/src/mobile/util/keyboardToolbar.ts
@@ -610,6 +610,11 @@ export const initKeyboardToolbar = () => {
             if (toolbarElement.clientHeight > 100) {
                 hideKeyboardToolbarUtil();
                 focusByRange(range);
+                if (window.JSAndroid && window.JSAndroid.showKeyboard) {
+                    window.JSAndroid.showKeyboard();
+                } else if (window.JSHarmony && window.JSHarmony.showKeyboard) {
+                    window.JSHarmony.showKeyboard();
+                }
             } else {
                 activeBlur();
             }

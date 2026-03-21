6775976348fe56ef7a2e4022cd1f3c05e6e880e7
Jeffrey Chen <78434827+TCOTC@users.noreply.github.com>
2026-03-21 23:35:15 +0800

    :art: Remove duplicate data-type attribute tags when converting inline elements to text https://github.com/siyuan-note/siyuan/issues/17292

--- app/src/protyle/toolbar/util.ts
+++ app/src/protyle/toolbar/util.ts
@@ -66,20 +66,14 @@ export const removeSearchMark = (element: HTMLElement) => {
 };
 
 export const removeInlineType = (inlineElement: HTMLElement, type: string, range?: Range) => {
-    const types = inlineElement.getAttribute("data-type").split(" ");
-    if (types.length === 1) {
+    const types = (inlineElement.getAttribute("data-type") || "").split(" ").filter((item) => item !== "" && item !== type);
+    if (types.length === 0) {
         const linkParentElement = inlineElement.parentElement;
         inlineElement.outerHTML = inlineElement.innerHTML.replace(Constants.ZWSP, "") + "<wbr>";
         if (range) {
             focusByWbr(linkParentElement, range);
         }
     } else {
-        types.find((itemType, index) => {
-            if (type === itemType) {
-                types.splice(index, 1);
-                return true;
-            }
-        });
         inlineElement.setAttribute("data-type", types.join(" "));
         if (type === "a") {
             inlineElement.removeAttribute("data-href");

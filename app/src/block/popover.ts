2e8ef4bf7a550565517e61bb16c9d6b6f0e7ff37
Vanessa <lly219@gmail.com>
2026-04-01 09:30:20 +0800

    :art: https://github.com/siyuan-note/siyuan/issues/17374

--- app/src/block/popover.ts
+++ app/src/block/popover.ts
@@ -84,7 +84,7 @@ export const initBlockPopover = (app: App) => {
             }
             let tooltipSpace: number | undefined;
             if (!tip && aElement.getAttribute("data-type")?.includes("inline-memo")) {
-                tip = aElement.getAttribute("data-inline-memo-content");
+                tip = window.DOMPurify.sanitize(aElement.getAttribute("data-inline-memo-content"));
                 tooltipClass = "memo"; // 为行级备注添加 class https://github.com/siyuan-note/siyuan/issues/6161
                 tooltipSpace = 0; // tooltip 和备注元素之间不能有空隙 https://github.com/siyuan-note/siyuan/issues/14796#issuecomment-3649757267
             }

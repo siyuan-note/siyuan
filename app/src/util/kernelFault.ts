34c7eb6c24ab508e4461d4ea3701fbf3af42a1c6
Daniel <845765@qq.com>
2026-06-24 08:46:55 +0800

    :art: Improve kernel disconnection error messages
    Signed-off-by: Daniel <845765@qq.com>

--- app/src/util/kernelFault.ts
+++ app/src/util/kernelFault.ts
@@ -1,6 +1,6 @@
 import {Constants} from "../constants";
 import {Dialog} from "../dialog";
-import {isMobile} from "./functions";
+import {isKernelInContainer, isMobile} from "./functions";
 import {isInIOS} from "../protyle/util/compatibility";
 
 export const kernelError = () => {
@@ -8,7 +8,16 @@ export const kernelError = () => {
         return;
     }
     let title = `💔 ${window.siyuan.languages.kernelFault0} <small>v${Constants.SIYUAN_VERSION}</small>`;
-    let body = `<div>${window.siyuan.languages.kernelFault1}</div><div class="fn__hr"></div><div>${window.siyuan.languages.kernelFault2}</div>`;
+    let body = `<div>${window.siyuan.languages.kernelFault1}</div>
+<div class="fn__hr"></div>
+<div>${window.siyuan.languages.kernelFault3}</div>
+<div>${isKernelInContainer() ? window.siyuan.languages.kernelFault4 : window.siyuan.languages.kernelFault5}</div>
+<div class="fn__hr"></div>
+<div>${window.siyuan.languages.kernelFault2}</div>
+<div class="b3-dialog__action">
+    <div class="fn__flex-1"></div>
+    <button class="b3-button">${window.siyuan.languages.kernelFault6}</button>
+</div>`;
     if (isInIOS()) {
         title = `🍵 ${window.siyuan.languages.pleaseWait} <small>v${Constants.SIYUAN_VERSION}</small>`;
         body = `<div>${window.siyuan.languages.reconnectPrompt}</div><div class="fn__hr"></div><div class="fn__flex"><div class="fn__flex-1"></div><button class="b3-button">${window.siyuan.languages.retry}</button></div>`;
@@ -25,11 +34,16 @@ export const kernelError = () => {
     });
     dialog.element.id = "errorLog";
     dialog.element.setAttribute("data-key", Constants.DIALOG_KERNELFAULT);
-    const restartElement = dialog.element.querySelector(".b3-button");
-    if (restartElement) {
-        restartElement.addEventListener("click", () => {
+    const btnsElement = dialog.element.querySelectorAll(".b3-button");
+    if (isInIOS()) {
+        btnsElement[0].addEventListener("click", () => {
             dialog.destroy();
             window.webkit.messageHandlers.startKernelFast.postMessage("startKernelFast");
+        });
+    } else {
+        btnsElement[0].addEventListener("click", () => {
+            dialog.destroy();
+            window.location.reload();
         });
     }
 };

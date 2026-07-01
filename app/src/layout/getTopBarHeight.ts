78a9cd7385c3a4300b77a1b6950c5c1159029bdb
Vanessa <lly219@gmail.com>
2026-07-01 22:03:39 +0800

    :art: https://github.com/siyuan-note/siyuan/issues/18024

--- app/src/layout/getTopBarHeight.ts
+++ app/src/layout/getTopBarHeight.ts
@@ -1,3 +1,3 @@
 export const getTopBarHeight = () => {
-    return  document.getElementById("sidebar") ? 0 : (document.getElementById("toolbar").clientHeight || document.querySelector(".layout-tab-bar").clientHeight);
+    return  document.getElementById("sidebar") ? 0 : (document.getElementById("toolbar")?.clientHeight || document.querySelector(".layout-tab-bar").clientHeight);
 };

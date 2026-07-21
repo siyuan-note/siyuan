7bb817cde692ae4a27e362129417b102e441e2a7
Nima Sharifkazemi <nima.nsk@hotmail.com>
2026-07-21 03:05:06 -0400

    feat(editor): type auto text direction preference

---
+++ app/src/types/autoTextDirection.d.ts
@@ -0,0 +1,6 @@
+declare namespace Config {
+    interface IEditor {
+        /** Whether to automatically detect text direction for supported editor blocks. */
+        autoTextDirection: boolean;
+    }
+}

fa87cdbcfc4ce49cb7e4a06d226806548c1ed269
Daniel <845765@qq.com>
2026-07-21 17:16:59 +0800

    :bug: Require SQLCipher for mobile kernel builds https://github.com/siyuan-note/siyuan/issues/18034

---
+++ kernel/mobile/sqlcipher_required.go
@@ -0,0 +1,22 @@
+// SiYuan - Refactor your thinking
+// Copyright (c) 2020-present, b3log.org
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+//go:build (android || ios) && !sqlcipher && !libsqlcipher
+
+package mobile
+
+// 移动端内核必须启用 SQLCipher，否则无法打开加密笔记本。
+var _ = sqlcipher_or_libsqlcipher_build_tag_required

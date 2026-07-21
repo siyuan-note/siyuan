15360edbd74e2f2e09e29392c434f202a0ed0f50
Nima Sharifkazemi <nima.nsk@hotmail.com>
2026-07-21 03:30:28 -0400

    ♻️ Remove nonconforming English code comment

--- app/src/types/autoTextDirection.d.ts
+++ app/src/types/autoTextDirection.d.ts
@@ -1,6 +1,5 @@
 declare namespace Config {
     interface IEditor {
-        /** Whether to automatically detect text direction for supported editor blocks. */
         autoTextDirection: boolean;
     }
 }

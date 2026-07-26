c1dc6ae56937a96548ef6158a6713047fe852571
Daniel <845765@qq.com>
2026-07-26 21:43:50 +0800

    :art: Remove redundant file tree event branches https://github.com/siyuan-note/siyuan/issues/12110

--- app/src/mobile/dock/MobileFiles.ts
+++ app/src/mobile/dock/MobileFiles.ts
@@ -522,8 +522,6 @@ export class MobileFiles extends Model {
                 case "moveDocs":
                     this.onMove(data);
                     break;
-                case "moveDoc":
-                    break;
                 case "reloadFiletree":
                     setNoteBook(() => {
                         this.init(false);

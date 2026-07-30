cd2c9f8d8c1b1a124aa1f3396260e83f60d3b5b4
Daniel <845765@qq.com>
2026-07-31 00:32:02 +0800

    :art: Focus the primary confirmation button by default https://github.com/siyuan-note/siyuan/issues/18460

--- app/src/dialog/confirmDialog.ts
+++ app/src/dialog/confirmDialog.ts
@@ -59,5 +59,5 @@ export const confirmDialog = (title: string, text: string,
         }
     });
     dialog.element.setAttribute("data-key", Constants.DIALOG_CONFIRM);
-    (dialog.element.querySelector("#cancelDialogConfirmBtn") as HTMLButtonElement).focus({preventScroll: true});
+    (dialog.element.querySelector("#confirmDialogConfirmBtn") as HTMLButtonElement).focus({preventScroll: true});
 };

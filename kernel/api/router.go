2c3b7af3f5fe5f93ad22a5af9cd14154855f6d9b
Daniel <845765@qq.com>
2026-08-04 10:01:32 +0800

    :lock: Restrict /api/lute/spinBlockDOM to admin role with input size limit https://github.com/siyuan-note/siyuan/security/advisories/GHSA-3j8q-5c8c-grwm

--- kernel/api/router.go
+++ kernel/api/router.go
@@ -206,7 +206,7 @@ func ServeAPI(ginServer *gin.Engine) {
 	ginServer.Handle("POST", "/api/tag/renameTag", model.CheckAuth, model.CheckAdminRole, model.CheckReadonly, renameTag)
 	ginServer.Handle("POST", "/api/tag/removeTag", model.CheckAuth, model.CheckAdminRole, model.CheckReadonly, removeTag)
 
-	ginServer.Handle("POST", "/api/lute/spinBlockDOM", model.CheckAuth, spinBlockDOM) // 未测试
+	ginServer.Handle("POST", "/api/lute/spinBlockDOM", model.CheckAuth, model.CheckAdminRole, spinBlockDOM)
 	ginServer.Handle("POST", "/api/lute/html2BlockDOM", model.CheckAuth, model.CheckAdminRole, model.CheckReadonly, html2BlockDOM)
 	ginServer.Handle("POST", "/api/lute/copyStdMarkdown", model.CheckAuth, copyStdMarkdown)
 	ginServer.Handle("POST", "/api/lute/md2html", model.CheckAuth, md2HTML)

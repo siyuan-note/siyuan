560fe5ac47d7833086ee4f9fceb431f5c51f74cc
Yingyi / 颖逸 <49649786+Zuoqiu-Yingyi@users.noreply.github.com>
2026-08-13 14:21:08 +0800

    :bug: Remove redundant TestInitPublishAccountsPreservesPluginJWT function to streamline tests

--- kernel/model/auth_test.go
+++ kernel/model/auth_test.go
@@ -101,30 +101,3 @@ func TestInitPublishAccountsWithNilAuth(t *testing.T) {
 		t.Fatal("InitPublishAccounts should default Publish.Auth when it is nil")
 	}
 }
-
-func TestInitPublishAccountsPreservesPluginJWT(t *testing.T) {
-	InitJwtKey()
-	pluginToken, err := CreatePluginJWT("test-plugin")
-	if err != nil {
-		t.Fatalf("CreatePluginJWT failed: %v", err)
-	}
-
-	InitJwtKey()
-	if _, err = ParseJWT(pluginToken); err != nil {
-		t.Fatalf("plugin JWT became invalid after repeated key initialization: %v", err)
-	}
-
-	originalConf := Conf
-	originalAccounts := accountsMap
-	defer func() {
-		Conf = originalConf
-		accountsMap = originalAccounts
-	}()
-
-	Conf = NewAppConf()
-	InitPublishAccounts()
-
-	if _, err = ParseJWT(pluginToken); err != nil {
-		t.Fatalf("plugin JWT became invalid after publish account initialization: %v", err)
-	}
-}

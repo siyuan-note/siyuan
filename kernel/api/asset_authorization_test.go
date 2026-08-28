c89a9b5dadf4cf768c66f9d5142b12037633cbe0
Daniel <845765@qq.com>
2026-08-28 20:46:56 +0800

    :bug: Prevent panic when pasting images in Agent chat https://github.com/siyuan-note/siyuan/issues/18973

--- kernel/api/asset_authorization_test.go
+++ kernel/api/asset_authorization_test.go
@@ -17,14 +17,70 @@
 package api
 
 import (
+	"bytes"
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
+	"os"
+	"path/filepath"
 	"strings"
 	"testing"
 
 	"github.com/gin-gonic/gin"
+	"github.com/siyuan-note/siyuan/kernel/conf"
 	"github.com/siyuan-note/siyuan/kernel/model"
+	"github.com/siyuan-note/siyuan/kernel/util"
 )
+
+func TestInsertLocalAssetsWithoutBlockID(t *testing.T) {
+	originalConf, originalWorkspaceDir, originalDataDir := model.Conf, util.WorkspaceDir, util.DataDir
+	workspaceDir := t.TempDir()
+	util.WorkspaceDir = workspaceDir
+	util.DataDir = filepath.Join(workspaceDir, "data")
+	model.Conf = model.NewAppConf()
+	model.Conf.Sync = conf.NewSync()
+	t.Cleanup(func() {
+		model.Conf = originalConf
+		util.WorkspaceDir = originalWorkspaceDir
+		util.DataDir = originalDataDir
+	})
+
+	assetPath := filepath.Join(util.DataDir, "assets", "pasted.png")
+	if err := os.MkdirAll(filepath.Dir(assetPath), 0755); err != nil {
+		t.Fatal(err)
+	}
+	if err := os.WriteFile(assetPath, []byte("image"), 0644); err != nil {
+		t.Fatal(err)
+	}
+	body, err := json.Marshal(map[string]any{
+		"assetPaths": []string{assetPath},
+		"isUpload":   true,
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	recorder := httptest.NewRecorder()
+	context, _ := gin.CreateTestContext(recorder)
+	request := httptest.NewRequest(http.MethodPost, "/api/asset/insertLocalAssets", bytes.NewReader(body))
+	request.Header.Set("Content-Type", "application/json")
+	context.Request = request
+	insertLocalAssets(context)
+
+	response := struct {
+		Code int `json:"code"`
+		Data struct {
+			SuccFiles []model.AssetUploadSuccess `json:"succFiles"`
+		} `json:"data"`
+	}{}
+	if err = json.Unmarshal(recorder.Body.Bytes(), &response); err != nil {
+		t.Fatal(err)
+	}
+	if response.Code != 0 || len(response.Data.SuccFiles) != 1 ||
+		response.Data.SuccFiles[0].Path != "assets/pasted.png" {
+		t.Fatalf("unexpected insert local assets response: %s", recorder.Body.String())
+	}
+}
 
 func TestAssetAdminEndpointsRejectReader(t *testing.T) {
 	gin.SetMode(gin.TestMode)

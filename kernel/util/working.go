a48cdf3ca62b7d499cdf54df9691d02d5306c9b7
Daniel <845765@qq.com>
2026-09-03 23:58:53 +0800

    :bookmark: Release v3.8.3-alpha.1
    Signed-off-by: Daniel <845765@qq.com>

--- kernel/util/working.go
+++ kernel/util/working.go
@@ -44,7 +44,7 @@ import (
 // var Mode = "dev"
 var Mode = "prod"
 
-const Ver = "3.8.2"
+const Ver = "3.8.3-alpha.1"
 
 // IsReleaseVer 判断是否为正式版（不含 alpha、beta、rc 等预发布标识）。
 func IsReleaseVer(ver string) bool {

1d44ddaf2e60c1f28dfaa5d537ce5e5736b5bcde
Daniel <845765@qq.com>
2026-09-12 20:39:45 +0800

    :bug: Clear snapshot progress overlay on indexing failure https://github.com/siyuan-note/siyuan/issues/19385

--- kernel/model/repository.go
+++ kernel/model/repository.go
@@ -1771,6 +1771,7 @@ func IndexRepo(memo string) (id string, err error) {
 	}
 
 	util.PushEndlessProgress(Conf.Language(143))
+	defer util.PushClearProgress()
 
 	start := time.Now()
 	latest, _ := repo.Latest()
@@ -1794,7 +1795,6 @@ func IndexRepo(memo string) (id string, err error) {
 		util.PushStatusBar(msg)
 		util.PushMsg(msg, 5000)
 	}
-	util.PushClearProgress()
 	return
 }
 

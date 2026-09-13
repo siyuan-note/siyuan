db70a62998535e67cf768473762a6fb923a57fe8
Daniel <845765@qq.com>
2026-09-13 20:23:23 +0800

    :bug: Preserve dock panel fullscreen in floating layouts https://github.com/siyuan-note/siyuan/issues/19438

--- app/src/layout/dock/index.ts
+++ app/src/layout/dock/index.ts
@@ -490,12 +490,16 @@ export class Dock {
             this.hideDock(true, preferredSize);
         } else {
             this.layout.element.style.transform = "";
+            this.layout.element.removeAttribute("data-temp");
             this.layout.element.style.zIndex = "";
             if (hasActive && this.panelVisible) {
                 this.resizeElement.classList.remove("fn__none");
             }
         }
         this.layout.element.classList.toggle("layout--float", this.isFloating());
+        if (this.isFloating() && this.layout.element.querySelector(".fullscreen")) {
+            this.showDock(true);
+        }
         if (!hasActive && !this.isFloating()) {
             this.layout.element.style[this.position === "Bottom" ? "height" : "width"] = "0px";
         }
@@ -600,6 +604,12 @@ export class Dock {
             this.layout.element.style.transform = `translateY(-${document.getElementById("status").offsetHeight}px)`;
             this.layout.element.style.left = this.elements[0].clientWidth + "px";
             this.layout.element.style.right = this.elements[1].clientWidth + "px";
+        }
+        // 全屏面板以窗口定位，浮动位移保留到退出全屏时恢复。
+        const fullscreenElement = this.layout.element.querySelector(".fullscreen");
+        if (fullscreenElement && fullscreenElement.clientHeight > 0) {
+            this.layout.element.setAttribute("data-temp", this.layout.element.style.transform);
+            this.layout.element.style.transform = "none";
         }
     }
 

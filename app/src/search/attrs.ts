4f2b9c5009c5d375b0c36ddf7165c6d201fbd10c
Daniel <845765@qq.com>
2026-09-15 17:24:47 +0800

    :bug: Show block attributes in mobile search results https://github.com/siyuan-note/siyuan/issues/19317

---
+++ app/src/search/attrs.ts
@@ -0,0 +1,13 @@
+export const getAttr = (block: Pick<IBlock, "name" | "alias" | "memo">) => {
+    let attrHTML = "";
+    if (block.name) {
+        attrHTML += `<span class="b3-list-item__meta fn__flex" style="max-width: 30%"><svg class="b3-list-item__hinticon"><use xlink:href="#iconN"></use></svg><span class="b3-list-item__hinttext">${block.name}</span></span>`;
+    }
+    if (block.alias) {
+        attrHTML += `<span class="b3-list-item__meta fn__flex" style="max-width: 30%"><svg class="b3-list-item__hinticon"><use xlink:href="#iconA"></use></svg><span class="b3-list-item__hinttext">${block.alias}</span></span>`;
+    }
+    if (block.memo) {
+        attrHTML += `<span class="b3-list-item__meta fn__flex" style="max-width: 30%"><svg class="b3-list-item__hinticon"><use xlink:href="#iconM"></use></svg><span class="b3-list-item__hinttext">${block.memo}</span></span>`;
+    }
+    return attrHTML;
+};

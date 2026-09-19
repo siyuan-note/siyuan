1c578260311950acee6ab9a2b5f3b83125f20ca2
Daniel <845765@qq.com>
2026-09-19 12:41:14 +0800

    :lock: Escape block attributes rendered in search result rows

--- app/src/search/attrs.ts
+++ app/src/search/attrs.ts
@@ -1,13 +1,15 @@
+import {escapeSearchHighlight} from "../util/escape";
+
 export const getAttr = (block: Pick<IBlock, "name" | "alias" | "memo">) => {
     let attrHTML = "";
     if (block.name) {
-        attrHTML += `<span class="b3-list-item__meta fn__flex" style="max-width: 30%"><svg class="b3-list-item__hinticon"><use xlink:href="#iconN"></use></svg><span class="b3-list-item__hinttext">${block.name}</span></span>`;
+        attrHTML += `<span class="b3-list-item__meta fn__flex" style="max-width: 30%"><svg class="b3-list-item__hinticon"><use xlink:href="#iconN"></use></svg><span class="b3-list-item__hinttext">${escapeSearchHighlight(block.name)}</span></span>`;
     }
     if (block.alias) {
-        attrHTML += `<span class="b3-list-item__meta fn__flex" style="max-width: 30%"><svg class="b3-list-item__hinticon"><use xlink:href="#iconA"></use></svg><span class="b3-list-item__hinttext">${block.alias}</span></span>`;
+        attrHTML += `<span class="b3-list-item__meta fn__flex" style="max-width: 30%"><svg class="b3-list-item__hinticon"><use xlink:href="#iconA"></use></svg><span class="b3-list-item__hinttext">${escapeSearchHighlight(block.alias)}</span></span>`;
     }
     if (block.memo) {
-        attrHTML += `<span class="b3-list-item__meta fn__flex" style="max-width: 30%"><svg class="b3-list-item__hinticon"><use xlink:href="#iconM"></use></svg><span class="b3-list-item__hinttext">${block.memo}</span></span>`;
+        attrHTML += `<span class="b3-list-item__meta fn__flex" style="max-width: 30%"><svg class="b3-list-item__hinticon"><use xlink:href="#iconM"></use></svg><span class="b3-list-item__hinttext">${escapeSearchHighlight(block.memo)}</span></span>`;
     }
     return attrHTML;
 };

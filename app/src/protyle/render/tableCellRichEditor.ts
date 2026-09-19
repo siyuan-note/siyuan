701dc7bc7d6adafb67ec75ee4a2e35188f7a50f5
Daniel <845765@qq.com>
2026-09-19 22:11:22 +0800

    :bug: Enable tag completion in table cell editors https://github.com/siyuan-note/siyuan/issues/19661

--- app/src/protyle/render/tableCellRichEditor.ts
+++ app/src/protyle/render/tableCellRichEditor.ts
@@ -1,6 +1,6 @@
 import {isMobile} from "../../util/functions";
 import {showMessage} from "../../dialog/message";
-import {hintRef, hintSlash} from "../hint/extend";
+import {hintRef, hintSlash, hintTag} from "../hint/extend";
 import {registerBuiltinSlashHint} from "../hint/builtinSlash";
 import {mountProtyleLiteFragment} from "../lite/fragmentEditor";
 import {setMobileToolbarUndo} from "../lite/mobileToolbar";
@@ -154,7 +154,7 @@ export const openTableCellRichEditor = (owner: IProtyle, cell: HTMLTableCellElem
         hintSlash(key, protyle, hintSource).filter(item => TABLE_CELL_SLASH_IDS.has(item.id)));
     const hint: IProtyleOptions["hint"] = {
         extend: [{key: "((", hint: hintRef}, {key: "【【", hint: hintRef}, {key: "（（", hint: hintRef},
-            {key: "[[", hint: hintRef}, {key: "/", hint: safeSlash}, {key: "、", hint: safeSlash}],
+            {key: "[[", hint: hintRef}, {key: "#", hint: hintTag}, {key: "/", hint: safeSlash}, {key: "、", hint: safeSlash}],
     };
     let finished = false;
     let composing = false;
